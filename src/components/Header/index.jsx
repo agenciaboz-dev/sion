a52ad0ab109a25333354299698b013cea8fdbb58
nandobfer <nandobfer@yahoo.com.br>
2023-02-14 09:22:42 -0300

    feat: added header menus

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,20 @@
+import { useHeaderMenus } from '../../hooks/useHeaderMenus';
 import './style.scss';
 
 export const Header = () => {
+
+    const menus = useHeaderMenus()
     
     return (
         <div className='Header-Component' >
-            Home
+            {menus.map(menu => {
+                return (
+                    <section key={menu.id}>
+                        <p>{menu.title}</p>
+                        {menus.indexOf(menu) == (menus.length - 1) ? null : <p>/</p>}
+                    </section>
+                )
+            })}
         </div>
     )
 }
---
+++ src/hooks/useHeaderMenus.js
@@ -0,0 +1,41 @@
+export const useHeaderMenus = () => {
+    const menus = [
+        {
+            id: 0,
+            title: 'Home',
+            name: 'home',
+        },
+        {
+            id: 1,
+            title: 'Quem somos',
+            name: 'about',
+        },
+        {
+            id: 2,
+            title: 'Para sua empresa',
+            name: 'for_your_company',
+        },
+        {
+            id: 3,
+            title: 'Blog',
+            name: 'blog',
+        },
+        {
+            id: 4,
+            title: 'Contato',
+            name: 'contact',
+        },
+        {
+            id: 5,
+            title: 'Parceiros',
+            name: 'partners',
+        },
+        {
+            id: 6,
+            title: 'Economize Agora',
+            name: 'save_now',
+        },
+    ]
+
+    return menus;
+}
