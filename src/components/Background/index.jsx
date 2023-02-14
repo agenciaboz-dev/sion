616f4a1408709efa35c305c9c99536f9e274bf22
nandobfer <nandobfer@yahoo.com.br>
2023-02-14 11:53:25 -0300

    feat: componentized background

---
+++ src/components/Background/index.jsx
@@ -0,0 +1,10 @@
+import './style.scss';
+
+export const Background = () => {
+    
+    return (
+        <div className='Background-Component' >
+            <img src="/images/vista-da-cidade-de-nova-york-eua.webp" alt="" />
+        </div>
+    )
+}
