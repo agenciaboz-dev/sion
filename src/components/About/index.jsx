144ab43ab4b81a36d43f24eef8b5efa802fa014c
nandobfer <nandobfer@yahoo.com.br>
2023-02-15 14:20:05 -0300

    update: general stylization

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -4,9 +4,12 @@ export const About = () => {
     
     return (
         <div className='About-Component' id='about' >
-            <img src="/images/about_building.webp" alt="" />
+            <div className="img-container">
+                <img src="/images/about_building.webp" alt="" />
+                <p>Sede Sion em Curitiba</p>
+            </div>
             <div className="main-container">
-                <h2>Quem somos</h2>
+                <h2>Quem somos?</h2>
                 <p>A Sion Energia é uma Holding com três Unidades de Negócio nas áreas de Geração e Comercialização de Energia.</p>
                 <p>Presente em todas as regiões do Brasil</p>
                 <p>+ 150 unidades consumidoras atendidas</p>
