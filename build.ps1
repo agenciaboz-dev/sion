98e86ca5300ec911d8f756b2d3193bad26f7d29f
nandobfer <nandobfer@yahoo.com.br>
2023-02-15 08:41:12 -0300

    update: gitignore

--- build.ps1
+++
@@ -1,4 +0,0 @@
-yarn build
-Write-Output 'Uploading build to server'
-scp -r -P 22022 build/* root@agenciazop.com.br:/home/cooperativasion/public_html/
-ssh -p 22022 root@agenciazop.com.br "chown -R cooperativasion:cooperativasion /home/cooperativasion/public_html/*"
