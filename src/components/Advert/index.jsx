c9ea64541a4587ba3f0d16cc57fbfae1a1861667
nandobfer <nandobfer@yahoo.com.br>
2023-02-15 11:03:51 -0300

    update: home anchor

--- src/components/Advert/index.jsx
+++ src/components/Advert/index.jsx
@@ -3,7 +3,7 @@ import './style.scss';
 export const Advert = ({ innerRef }) => {
     
     return (
-        <div className='Advert-Component' id='home' ref={innerRef}>
+        <div className='Advert-Component' ref={innerRef}>
             <h1>Até 15%</h1>
             <h2>de economia na sua conta de luz!</h2>
             <p>Sem Investimento Sem instalação Adesão 100% online Uso de Energia</p>
