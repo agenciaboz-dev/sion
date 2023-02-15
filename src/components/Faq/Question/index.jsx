b6f1a2007166fd8e1943d066ffca3d91c153f073
nandobfer <nandobfer@yahoo.com.br>
2023-02-15 12:16:05 -0300

    feat: added collapsible faq

--- src/components/Faq/Question/index.jsx
+++ src/components/Faq/Question/index.jsx
@@ -1,14 +1,32 @@
+import Collapsible from 'react-collapsible'
 import {ReactComponent as Arrow} from '../../../images/faq_arrow.svg'
+
+const QuestionBlock = ({ question, open }) => {
+    return (
+        <div className="question">
+            <Arrow style={open ? {transform: 'rotate(90deg)'} : null} />
+            <h3>{question.number}. {question.text}</h3>
+        </div>
+    )
+}
+
+const AnswerBlock = ({ question }) => {
+    return (
+        <div className="answer">
+                <p>{question.answer}</p>
+        </div>
+    )
+}
 
 export const Question = ({ question }) => {
     
     return (
         <div className='Question-Component' >
-            <div className="question">
-                <Arrow />
-                <h3>{question.number}. {question.text}</h3>
-            </div>
-            <p>{question.answer}</p>
+            <Collapsible trigger={<QuestionBlock question={question} />}
+                triggerWhenOpen={<QuestionBlock question={question} open={true} />}
+            >
+                <AnswerBlock question={question} />
+            </Collapsible>
         </div>
     )
 }
