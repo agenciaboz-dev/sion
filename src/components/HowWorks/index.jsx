9ac4201810cee6badf10b49c11c3ba5854a9f322
nandobfer <nandobfer@yahoo.com.br>
2023-02-15 13:27:10 -0300

    feat: diagonal background divs

--- src/components/HowWorks/index.jsx
+++ src/components/HowWorks/index.jsx
@@ -1,15 +1,19 @@
 import { useEffect, useState } from 'react';
+import useMeasure from 'react-use-measure';
 import { HowItem } from './HowItem';
 import './style.scss';
 
 export const HowWorks = ({ main_container_height, advert_height }) => {
-    const [backgroundHeight, setBackgroundHeight] = useState(0)
+
+    const [ref, {height}] = useMeasure()
 
     return (
-        <div className='HowWorks-Component' id='how' >
+        <div className='HowWorks-Component' id='how' ref={ref} >
             <div className="background-container" style={{height: main_container_height - advert_height}}>
 
             </div>
+            <div className="blue-background"></div>
+            <div className="white-background" style={{height: height * 1.2}}></div>
             <h1>Como funciona?</h1>
 
             <hr />
