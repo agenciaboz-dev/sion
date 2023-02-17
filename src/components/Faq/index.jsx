5b07d4283ed415fb3007fd2754504c42ad7f0596
ispadilha <israel.padilha@gmail.com>
2023-02-17 10:34:16 -0300

    various stylings on the home page

--- src/components/Faq/index.jsx
+++ src/components/Faq/index.jsx
@@ -8,12 +8,50 @@ export const Faq = () => {
     
     return (
         <div className='Faq-Component' id='faq' >
-            <h2>Dúvidas frequentes</h2>
-            {questions.map(question => {
-                return (
-                    <Question key={question.number} question={question} />
-                )
-            })}
+            <div className="faq-top-container">
+                <div className="faq-header-container">
+                    <h2>Dúvidas frequentes</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+                {/* {questions.map(question => {
+                    return (
+                        <Question key={question.number} question={question} />
+                    )
+                })} */}
+                <div className="questions-container">
+                    <div className="qcolumn">
+                        <div className="question-answer">
+                            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <div className="question-answer">
+                            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                    </div>
+                    <div className="qcolumn">
+                        <div className="question-answer">
+                            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <div className="question-answer">
+                            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                    </div>
+                    <div className="qcolumn">
+                        <div className="question-answer">
+                            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <div className="question-answer">
+                            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <div className="contact">
                 <h4>Ficou com mais alguma dúvida? Fale com um dos nossos assessores</h4>
                 <button>Entrar em contato</button>
--- src/components/Simulator/index.jsx
+++ src/components/Simulator/index.jsx
@@ -30,7 +30,6 @@ export const Simulator = () => {
 
     return (
         <div className='Simulator-Component' id='simulator' >
-            <div className="inset-background"></div>
             <div className="left-container">
                 <Benefits title={'Economia'} text='Receba seus créditos de energia e economize em até 15% na sua fatura' />
                 <Benefits title={'Sem investimento'} text='Sem necessidade de alteração física no seu negócio ou investimento' />
