c3718677a2abbc1e2e36774bc614e7a1ca682262
nandobfer <nandobfer@yahoo.com.br>
2023-02-17 08:35:37 -0300

    update: faq questions

--- src/hooks/useFaq.js
+++ src/hooks/useFaq.js
@@ -2,7 +2,7 @@ export const useFaq = () => {
     const questions = [
         {
             number: 1,
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, consectetur adipiscing elit.',
             answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
         },
         {
@@ -12,12 +12,12 @@ export const useFaq = () => {
         },
         {
             number: 3,
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            text: 'Lorem ipsum dolor sit amet.',
             answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
         },
         {
             number: 4,
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, consectetur adipiscing elit, consectetur adipiscing elit',
             answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
         },
         {
