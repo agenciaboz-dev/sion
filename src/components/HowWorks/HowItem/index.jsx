5731c25253e66c642c436e200c615a1759af5533
ispadilha <israel.padilha@gmail.com>
2023-02-28 10:32:30 -0300

    preparing new icons

--- src/components/HowWorks/HowItem/index.jsx
+++ src/components/HowWorks/HowItem/index.jsx
@@ -1,11 +1,10 @@
-import {ReactComponent as Icon} from '../../../images/how_item.svg';
 
-export const HowItem = ({ right, title, text, }) => {
+export const HowItem = ({ right, title, text, icon }) => {
     
     return (
         <div className='HowItem-Component' >
             <div className="left">{right ? <h3>{title}</h3> : <p>{text}</p>}</div>
-            <Icon className="how-item-icon"/>
+            {icon}
             <div className="right">{right ? <p>{text}</p> : <h3>{title}</h3>}</div>
         </div>
     )
--- src/components/HowWorks/index.jsx
+++ src/components/HowWorks/index.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import useMeasure from 'react-use-measure';
 import { HowItem } from './HowItem';
 import './style.scss';
+import {ReactComponent as Icon} from '../../images/how_item.svg';
 
 export const HowWorks = ({ main_container_height, advert_height }) => {
 
@@ -15,15 +16,15 @@ export const HowWorks = ({ main_container_height, advert_height }) => {
             <div className="blue-background"></div>
             <div className="white-background" style={{height: height * 1.05}}></div>
             <h1>Como funciona?</h1>
-
+            
             <hr />
-            <HowItem right title='Energia Renovável' text='Nossa empresa tem se destacado na geração de energia renovável, oferecendo preços mais baixos em comparação às distribuidoras convencionais. Utilizando fontes renováveis e tecnologias inovadoras, conseguimos maximizar a produção de energia e reduzir custos operacionais.' />
+            <HowItem icon={() => <Icon className="how-item-icon" />} right title='Energia Renovável' text='Nossa empresa tem se destacado na geração de energia renovável, oferecendo preços mais baixos em comparação às distribuidoras convencionais. Utilizando fontes renováveis e tecnologias inovadoras, conseguimos maximizar a produção de energia e reduzir custos operacionais.' />
             <hr />
-            <HowItem title='Convertida em Créditos' text='A energia produzida é injetada na rede e convertida em créditos de energia, que podem ser usados para reduzir a conta de luz do proprietário. O sistema de geração distribuída torna-se uma alternativa econômica e sustentável para os consumidores, permitindo que economizem em suas contas de luz.' />
+            <HowItem icon={() => <Icon className="how-item-icon" />} title='Convertida em Créditos' text='A energia produzida é injetada na rede e convertida em créditos de energia, que podem ser usados para reduzir a conta de luz do proprietário. O sistema de geração distribuída torna-se uma alternativa econômica e sustentável para os consumidores, permitindo que economizem em suas contas de luz.' />
             <hr />
-            <HowItem right title='Distribuida aos consumidores' text='Após a conversão da energia injetada em créditos de energia, distribuímos esses créditos entre os consumidores. Em seguida, emitimos a cobrança com o desconto de até 15%, baseado na quantidade de créditos gerados e utilizados pelo cliente.' />
+            <HowItem icon={() => <Icon className="how-item-icon" />} right title='Distribuida aos consumidores' text='Após a conversão da energia injetada em créditos de energia, distribuímos esses créditos entre os consumidores. Em seguida, emitimos a cobrança com o desconto de até 15%, baseado na quantidade de créditos gerados e utilizados pelo cliente.' />
             <hr />
-            <HowItem title='Gerando desconto de até 15% na fatura' text='Com base na quantidade de créditos gerados e utilizados, a fatura da distribuidora é automaticamente ajustada, com desconto de até 15%. Esse desconto representa uma economia significativa para nossos cooperados e é possível graças ao valor de mercado dos créditos de energia.' />
+            <HowItem icon={() => <Icon className="how-item-icon" />} title='Gerando desconto de até 15% na fatura' text='Com base na quantidade de créditos gerados e utilizados, a fatura da distribuidora é automaticamente ajustada, com desconto de até 15%. Esse desconto representa uma economia significativa para nossos cooperados e é possível graças ao valor de mercado dos créditos de energia.' />
         </div>
     )
 }
