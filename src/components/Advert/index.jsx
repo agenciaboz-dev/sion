d8af09b127f96ef8b827395facd703f6b4e5da9a
nandobfer <nandobfer@yahoo.com.br>
2023-02-28 11:46:44 -0300

    feat: added cadastro

--- src/components/Advert/index.jsx
+++ src/components/Advert/index.jsx
@@ -1,7 +1,9 @@
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { useNavigate } from 'react-router-dom';
 import './style.scss';
 
 export const Advert = ({ innerRef }) => {
+    const navigate = useNavigate()
     
     return (
         <div className='Advert-Component' ref={innerRef}>
@@ -10,7 +12,7 @@ export const Advert = ({ innerRef }) => {
             <h2 className='shadow-text'>Aumente sua rentabilidade com nossa <span>energia renovável</span> e<br />de <span>menor custo</span> para sua empresa</h2>
             <p className='shadow-text'>Sem Investimento Sem instalação Adesão 100% online Uso de Energia Renovável</p>
             <div className="buttons">
-                <button>Quero economizar!</button>
+                <button onClick={() => navigate('/cadastro')}>Quero economizar!</button>
                 <AnchorLink href={'#how'}className='menu-title'>Saiba como funciona</AnchorLink>
             </div>
         </div>
