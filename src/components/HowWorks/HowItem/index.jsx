096e08b588a8836ea8c1987c489857df79f57cf4
ispadilha <israel.padilha@gmail.com>
2023-02-28 09:57:32 -0300

    update: how-works component texts

--- src/components/HowWorks/HowItem/index.jsx
+++ src/components/HowWorks/HowItem/index.jsx
@@ -1,11 +1,11 @@
 import {ReactComponent as Icon} from '../../../images/how_item.svg';
 
-export const HowItem = ({ right, title, text }) => {
+export const HowItem = ({ right, title, text, }) => {
     
     return (
         <div className='HowItem-Component' >
             <div className="left">{right ? <h3>{title}</h3> : <p>{text}</p>}</div>
-            <Icon />
+            <Icon className="how-item-icon"/>
             <div className="right">{right ? <p>{text}</p> : <h3>{title}</h3>}</div>
         </div>
     )
--- src/components/HowWorks/index.jsx
+++ src/components/HowWorks/index.jsx
@@ -13,17 +13,17 @@ export const HowWorks = ({ main_container_height, advert_height }) => {
 
             </div>
             <div className="blue-background"></div>
-            <div className="white-background" style={{height: height/* * 1.1*/}}></div>
+            <div className="white-background" style={{height: height * 1.05}}></div>
             <h1>Como funciona?</h1>
 
             <hr />
-            <HowItem right title='Lorem ipsum dolor sit' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' />
+            <HowItem right title='Energia Renovável' text='Nossa empresa tem se destacado na geração de energia renovável, oferecendo preços mais baixos em comparação às distribuidoras convencionais. Utilizando fontes renováveis e tecnologias inovadoras, conseguimos maximizar a produção de energia e reduzir custos operacionais.' />
             <hr />
-            <HowItem title='Lorem ipsum dolor sit' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' />
+            <HowItem title='Convertida em Créditos' text='A energia produzida é injetada na rede e convertida em créditos de energia, que podem ser usados para reduzir a conta de luz do proprietário. O sistema de geração distribuída torna-se uma alternativa econômica e sustentável para os consumidores, permitindo que economizem em suas contas de luz.' />
             <hr />
-            <HowItem right title='Lorem ipsum dolor sit' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' />
+            <HowItem right title='Distribuida aos consumidores' text='Após a conversão da energia injetada em créditos de energia, distribuímos esses créditos entre os consumidores. Em seguida, emitimos a cobrança com o desconto de até 15%, baseado na quantidade de créditos gerados e utilizados pelo cliente.' />
             <hr />
-            <HowItem title='Lorem ipsum dolor sit' text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' />
+            <HowItem title='Gerando desconto de até 15% na fatura' text='Com base na quantidade de créditos gerados e utilizados, a fatura da distribuidora é automaticamente ajustada, com desconto de até 15%. Esse desconto representa uma economia significativa para nossos cooperados e é possível graças ao valor de mercado dos créditos de energia.' />
         </div>
     )
 }
