d792c08748c833bc104c98a65c5abf8239d2c588
ispadilha <israel.padilha@gmail.com>
2023-03-13 12:00:00 -0300

    feat: started creating whatsapp button

---
+++ src/components/WhatsappButton/index.jsx
@@ -0,0 +1,13 @@
+import './style.scss';
+
+export const WhatsappButton = () => {
+
+    return(
+
+        <div className="WhatsappButton-Component">
+            <h5>whatsapp</h5>
+        </div>
+
+    )
+
+}
