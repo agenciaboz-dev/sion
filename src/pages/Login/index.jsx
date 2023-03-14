095ccfa43eb0882421c944d52d5eae3acd9e5321
nandobfer <nandobfer@yahoo.com.br>
2023-03-14 11:11:33 -0300

    feat: added login page

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,14 @@
+import { Footer } from '../../components/Footer';
+import './style.scss';
+
+export const Login = () => {
+    
+    return (
+        <div className='Login-Page' >
+            <div className="main-container">
+                
+            </div>
+            <Footer />
+        </div>
+    )
+}
