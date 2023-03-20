b54e145565b3102de997b20a08ef31910de2ceaa
ispadilha <israel.padilha@gmail.com>
2023-03-20 11:29:27 -0300

    update: about-us section component

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -9,12 +9,14 @@ export const About = () => {
                 <p>Sede Sion em Curitiba</p>
             </div>
             <div className="main-container">
-                <h2>Quem somos?</h2>
+                <h2 className='about-us'>SOBRE NÓS</h2>
+                <h2>A Sion Energia</h2>
                 <p>A Sion Energia é uma Holding com três Unidades de Negócio nas áreas de Geração e Comercialização de Energia.</p>
-                <p>Presente em todas as regiões do Brasil</p>
-                <p>+ 150 unidades consumidoras atendidas</p>
-                <p>+ 5,2 milhões de kWh mensais em nossa Gestão</p>
-                <p>+ 6 milhões de m² entre usinas construídas e em desenvolvimento</p>
+                <p>A Sion Geração desenvolve e faz a operação de usinas. A Sion Gestão é uma gestora de energia de grandes consumidores e a Cooperativa de Energia atende empresas do Varejo.</p>
+                <p className='about-item'>● Presente em todas as regiões do Brasil</p>
+                <p className='about-item'>● 150 unidades consumidoras atendidas</p>
+                <p className='about-item'>● 5,2 milhões de kWh mensais em nossa Gestão</p>
+                <p className='about-item'>● 6 milhões de m² entre usinas construídas e em desenvolvimento</p>
             </div>
         </div>
     )
