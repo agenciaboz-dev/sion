ad46e9637a1093096fb2df699792dd5e267f62e6
nandobfer <nandobfer@yahoo.com.br>
2023-03-21 19:14:02 -0300

    fix: mobile background

--- src/components/BackgroundContainer/index.jsx
+++ src/components/BackgroundContainer/index.jsx
@@ -3,13 +3,19 @@ import { useState } from 'react';
 import { Background } from '../Background';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
+import { useMediaQuery } from 'react-responsive';
 
 export const BackgroundContainer = ({ children }) => {
     const [stickyHeader, setstickyHeader] = useState(false)
+    const isMobile = useMediaQuery({maxWidth: 600})
+
+    const style = {
+        top: isMobile ? 0 : '6.5vw' 
+    }
     
     return (
         <div className='BackgroundContainer-Component' >
-            <Background style={{top: '6.5vw'}} />
+            <Background style={style} />
             <Header alternative={stickyHeader} setAlternative={setstickyHeader} />
 
             <div className="children-container">
