e9a78ebfd90628222b207361ea7f519e0895f377
nandobfer <nandobfer@yahoo.com.br>
2023-03-22 12:03:04 -0300

    feat: added flags calculation to calcutador

---
+++ src/hooks/useFlags.js
@@ -0,0 +1,26 @@
+export const useFlags = () => {
+    const flags = [
+        {
+            name: 'Sem bandeira',
+            factor: 1,
+        },
+        {
+            name: 'Verde',
+            factor: 0.82,
+        },
+        {
+            name: 'Amarela',
+            factor: 1.07,
+        },
+        {
+            name: 'Vermelha 1',
+            factor: 1.36,
+        },
+        {
+            name: 'Vermelha 2',
+            factor: 1.79,
+        },
+    ]
+
+    return flags
+}
--- src/pages/Cadastro/Calculadora/index.jsx
+++ src/pages/Cadastro/Calculadora/index.jsx
@@ -8,46 +8,20 @@ import { useCurrencyMask } from '../../../hooks/useCurrencyMask';
 import { usePercentMask } from '../../../hooks/usePercentMask';
 import './style.scss';
 import COLORS from '../../../sass/_colors.scss'
+import { useFlags } from '../../../hooks/useFlags';
 
 export const Calculadora = () => {
-
-    const Flags = () => {
-        const onSubmit = values => {
-            // if (!client.value.anexos) {
-            //     setFileError(true)
-            //     return
-            // }
-            client.setValue({...client.value, flag: values.flag})
-            navigate('/cadastro/calculadora')
-        }
-
-        return (
-            <Formik initialValues={{flag: 0}} onSubmit={onSubmit} innerRef={flagRef} >
-                {({values, handleChange}) => (
-                    <Form>
-                        <InputField select id='flag' title='Bandeira' handleChange={handleChange} value={values.flag} not_required >
-                            <MenuItem
-                                value={0}
-                                style={{width: '100%'}}
-                            >Sem bandeira</MenuItem>
-                            <MenuItem
-                                value={1}
-                                style={{width: '100%'}}
-                            >Verde</MenuItem>
-                        </InputField>
-                    </Form>
-                )}
-            </Formik>
-        )
-    }
 
     const client = useClient()
     const navigate = useNavigate()
     const flagRef = useRef(null)
     const percentMask = usePercentMask()
     const currencyMask = useCurrencyMask()
+    const flags = useFlags()
 
     const [spent, setSpent] = useState(client.value.form?.monthly_spent || 'R$ 1.000')
+    const [currentFlag, setCurrentFlag] = useState(1)
+    const [newFlag, setNewFlag] = useState(1)
     const [discount, setDiscount] = useState('')
     const [new_spent, setNew_spent] = useState('')
     const [profit, setProfit] = useState('')
@@ -62,33 +36,43 @@ export const Calculadora = () => {
     }
 
     useEffect(() => {
-        const spent_number = parseFloat(spent.replace(/(R\$\s*)|(\.)/g, "").replace(",", "."))
+        const spent_number = parseFloat(spent.replace(/(R\$\s*)|(\.)/g, "").replace(",", ".")) / currentFlag
         const discount_number = parseFloat(discount.replace(/(%\s*)|(\.)/g, "").replace(",", "."))
+        const flagged_spent = spent_number * newFlag
         
-        const new_spent_number = spent_number - (spent_number * (discount_number / 100))
+        const new_spent_number = flagged_spent - (flagged_spent * (discount_number / 100))
         const new_spent_string = new_spent_number.toString().replace('.', ',')
         
         setNew_spent(new_spent_string)
-    }, [spent, discount])
+    }, [spent, discount, currentFlag, newFlag])
 
     useEffect(() => {
-        const spent_number = parseFloat(spent.replace(/(R\$\s*)|(\.)/g, "").replace(",", "."))
+        const spent_number = parseFloat(spent.replace(/(R\$\s*)|(\.)/g, "").replace(",", ".")) / currentFlag
         const discount_number = parseFloat(discount.replace(/(%\s*)|(\.)/g, "").replace(",", "."))
+        const flagged_spent = spent_number * newFlag
         
-        const new_spent_number = spent_number * (discount_number / 100)
+        const new_spent_number = flagged_spent * (discount_number / 100)
         const profit_number = parseFloat(profit.replace(/(%\s*)|(\.)/g, "").replace(",", "."))
 
         const cost_number = new_spent_number * (profit_number / 100)
         const cost_string = cost_number.toString().replace('.', ',')
         
         setCost(cost_string)
-    }, [profit, new_spent])
+    }, [profit, new_spent, currentFlag, newFlag])
     
     return (
         <div className='Calculadora-Component' >
             <h1>Calculadora de economia</h1>
-            <Flags />
+            <InputField select title='Bandeira' handleChange={event => setCurrentFlag(event.target.value)} value={currentFlag} not_required >
+                {flags.map(flag => <MenuItem key={flag.factor} value={flag.factor} style={{width: '100%'}} >{flag.name}</MenuItem>)}
+            </InputField>
+
             <InputField title={'Gasto Mensal'} id={'spent'} value={spent} readOnly not_required />
+
+            <InputField select title='Projeção de bandeira' handleChange={event => setNewFlag(event.target.value)} value={newFlag} not_required >
+                {flags.map(flag => <MenuItem key={flag.factor} value={flag.factor} style={{width: '100%'}} >{flag.name}</MenuItem>)}
+            </InputField>
+
             <InputField mask={percentMask} title={'Desconto'} id={'discount'} value={discount} handleChange={event => setDiscount(event.target.value)} />
             <InputField mask={currencyMask} title={'Novo valor da fatura'} id={'new_spent'} value={new_spent || 0} readOnly not_required />
             <InputField mask={percentMask} title={'Taxa sion'} id={'profit'} value={profit} handleChange={event => setProfit(event.target.value)} />
