5a74fdda810199d1aec3301fca073bf4c073fb53
nandobfer <nandobfer@yahoo.com.br>
2023-03-22 19:45:33 -0300

    feat: added api post contract

--- src/pages/Cadastro/Contrato/index.jsx
+++ src/pages/Cadastro/Contrato/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { api } from '../../../api';
 import { useClient } from '../../../hooks/useClient';
 import {ReactComponent as ChoseIcon} from '../../../images/blue_check.svg'
 import './style.scss';
@@ -17,6 +18,14 @@ export const Contrato = ({ setProgressBarStage, setStage }) => {
     useEffect(() => {
         setProgressBarStage(100)
         setStage(2)
+
+        const data = {...client.value, ...client.value.form}
+        delete data.form
+
+        api.post('/contract/new', data)
+        .then(response => console.log(response.data))
+        .catch(error => console.error(error))
+        
 
     }, [])
     
