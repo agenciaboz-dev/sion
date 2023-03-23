c6265780f44cadf35a6089b90ff10047a30a5c84
nandobfer <nandobfer@yahoo.com.br>
2023-03-23 12:31:42 -0300

    update: hardcoded decimal and thousands separator

--- src/hooks/useCurrencyMask.js
+++ src/hooks/useCurrencyMask.js
@@ -1,20 +1,13 @@
 import createNumberMask from 'text-mask-addons/dist/createNumberMask';
 
 export const useCurrencyMask = () => {
-    const locale = navigator.language || 'en-US';
-
-    // Get thousands separator and decimal point from locale
-    const formatter = new Intl.NumberFormat(locale);
-    const parts = formatter.formatToParts(1234.56);
-    const thousandsSeparator = parts.find(part => part.type === 'group').value;
-    const decimalSeparator = parts.find(part => part.type === 'decimal').value;
 
     // Create number mask with variable thousands separator and decimal point
     const currencyMask = createNumberMask({
         prefix: 'R$ ',
         suffix: '',
-        thousandsSeparatorSymbol: thousandsSeparator,
-        decimalSymbol: decimalSeparator,
+        thousandsSeparatorSymbol: '.',
+        decimalSymbol: ',',
         allowDecimal: true,
         decimalLimit: 2,
     });
