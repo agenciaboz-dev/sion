4246f7f8d6db32ca4200bc6f97609aecef25049c
nandobfer <nandobfer@yahoo.com.br>
2023-03-22 23:26:36 -0300

    update: build

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 yarn build
 Write-Output 'Uploading build to server'
-scp -r -P 22022 build/* root@agenciaboz.com.br:/home/cooperativasion/vendas.cooperativasion.com.br
-ssh -p 22022 root@agenciaboz.com.br "chown -R cooperativasion:cooperativasion /home/cooperativasion/vendas.cooperativasion.com.br/*"
+scp -r -P 22022 build/* agenciaboz:/home/cooperativasion/vendas.cooperativasion.com.br
+ssh -p 22022 agenciaboz "chown -R cooperativasion:cooperativasion /home/cooperativasion/vendas.cooperativasion.com.br/*"
