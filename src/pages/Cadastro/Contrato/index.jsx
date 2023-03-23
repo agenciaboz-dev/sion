acf92c91e3bcd5b525b50de642157131bec4406b
nandobfer <nandobfer@yahoo.com.br>
2023-03-22 21:21:50 -0300

    feat: added loading to contract page

--- src/pages/Cadastro/Contrato/index.jsx
+++ src/pages/Cadastro/Contrato/index.jsx
@@ -1,6 +1,7 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { api } from '../../../api';
+import { MuiLoading } from '../../../components/MuiLoading';
 import { useClient } from '../../../hooks/useClient';
 import {ReactComponent as ChoseIcon} from '../../../images/blue_check.svg'
 import './style.scss';
@@ -9,6 +10,8 @@ export const Contrato = ({ setProgressBarStage, setStage }) => {
 
     const navigate = useNavigate()
     const client = useClient()
+
+    const [loading, setLoading] = useState(true)
 
     const finish = () => {
         client.setValue(null)
@@ -25,20 +28,29 @@ export const Contrato = ({ setProgressBarStage, setStage }) => {
         api.post('/contract/new', data)
         .then(response => console.log(response.data))
         .catch(error => console.error(error))
-        
+        .finally(() => setLoading(false))
+
 
     }, [])
     
     return (
         <div className='Contrato-Component' >
             <div className="main-container">
-                <ChoseIcon style={{height:'11vw', width: '11vw'}} />
-                <h1>Tudo pronto!</h1>
-                <div className='description-container'>
-                    <p>Foi enviado um email com as</p>
-                    <p>informações recebidas!</p>
+                {loading ?
+                <div className="loading-container">
+                    <MuiLoading color={'primary'} size={'15vw'} />
                 </div>
-                <button onClick={() => finish()}>Finalizar</button>
+                :
+                <section>
+                    <ChoseIcon style={{height:'11vw', width: '11vw'}} />
+                    <h1>Tudo pronto!</h1>
+                    <div className='description-container'>
+                        <p>Foi enviado um email com as</p>
+                        <p>informações recebidas!</p>
+                    </div>
+                    <button onClick={() => finish()}>Finalizar</button>
+                </section>
+                }
             </div>
 
             
