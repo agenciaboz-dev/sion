4bbb034e75bb5f4581310d36d6e3f9b16f921d74
nandobfer <nandobfer@yahoo.com.br>
2023-03-28 09:51:07 -0300

    feat: added cnpj validation

--- src/pages/Cadastro/Formulario/PessoaJuridica/index.jsx
+++ src/pages/Cadastro/Formulario/PessoaJuridica/index.jsx
@@ -4,10 +4,15 @@ import { useValidationErrors } from '../../../../hooks/useValidationsErrors';
 import { InputField } from '../../../../components/InputField';
 import COLORS from '../../../../sass/_colors.scss'
 import { useClient } from '../../../../hooks/useClient';
+import { useState } from 'react';
+import { useValidateCNPJ } from '../../../../hooks/useValidateCNPJ';
 
 export const PessoaJuridica = ({ previousStage, nextStage }) => {
     const default_errors = useValidationErrors()
     const client = useClient()
+    const validateCNPJ = useValidateCNPJ()
+    
+    const [cnpjError, setCnpjError] = useState(false)
 
     const initial_inputs = client.value?.form ||  {
         company: '',
@@ -28,10 +33,15 @@ export const PessoaJuridica = ({ previousStage, nextStage }) => {
         // email: string().email(default_errors.email),
         // website: string().url().nullable(),
         // createdOn: date().default(() => new Date()),
-      });
+    })
+
+    // 12.988.742/0001-56
+    const cnpjBlur = event => {
+        setCnpjError(!validateCNPJ(event.target.value))
+    }
 
     return (
-        <Formik initialValues={initial_inputs} onSubmit={values => nextStage(values)} validationSchema={validationSchema}>
+        <Formik initialValues={initial_inputs} onSubmit={nextStage} validationSchema={validationSchema}>
             {({handleChange, values, submitForm, errors}) => (
                 <Form onKeyDown={(event) => {
                     if (event.key === 'Enter') {
@@ -40,7 +50,7 @@ export const PessoaJuridica = ({ previousStage, nextStage }) => {
                     }
                     }}>
                     <InputField title={'Razão Social'} id={'company'} handleChange={handleChange} value={values.company} error={Boolean(errors.company)} errorText={errors.company} />
-                    <InputField title={'CNPJ'} mask={[/\d/,/\d/,'.',/\d/,/\d/,/\d/,'.',/\d/,/\d/,/\d/,'/',/\d/,/\d/,/\d/,/\d/,'-',/\d/,/\d/]} inputMode={'numeric'} id={'cnpj'} handleChange={handleChange} value={values.cnpj} error={Boolean(errors.cnpj)} errorText={errors.cnpj} />
+                    <InputField title={'CNPJ'} onBlur={cnpjBlur} mask={[/\d/,/\d/,'.',/\d/,/\d/,/\d/,'.',/\d/,/\d/,/\d/,'/',/\d/,/\d/,/\d/,/\d/,'-',/\d/,/\d/]} inputMode={'numeric'} id={'cnpj'} handleChange={handleChange} value={values.cnpj} error={cnpjError} errorText={'CNPJ inválido'} />
                     <InputField title={'Ramo de atividade'} id={'category'} handleChange={handleChange} value={values.category} error={Boolean(errors.category)} errorText={errors.category} />
                     <InputField title={'Endereço da unidade consumidora'} id={'address'} handleChange={handleChange} value={values.address} error={Boolean(errors.address)} errorText={errors.address} />
                     <InputField title={'CEP'} inputMode={'numeric'} mask={[/\d/, /\d/, '.', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/]} id={'cep'} handleChange={handleChange} value={values.cep} error={Boolean(errors.cep)} errorText={errors.cep} />
