2e33bcdb7601fc904901a140d8f350babc63d9a2
ispadilha <israel.padilha@gmail.com>
2023-04-03 09:41:35 -0300

    update: links page

--- src/pages/Links/index.jsx
+++ src/pages/Links/index.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { BackgroundContainer } from '../../components/BackgroundContainer';
+import {ReactComponent as LogoBranco} from '../../images/logo_branco.svg';
 import { ReactComponent as InstagramIcon } from '../../images/socials/instagram.svg'
 import { ReactComponent as FacebookIcon } from '../../images/socials/facebook.svg'
 import { ReactComponent as WhatsappIcon } from '../../images/socials/whatsapp.svg'
@@ -18,14 +19,20 @@ export const Links = () => {
         <div className='Links-Page' >
             <BackgroundContainer>
                 <div className="main-container">
-                    <button onClick={() => navigate('/')}>Site</button>
-                    <button onClick={() => navigate('/')}>Simulador de Economia</button>
-                    <button onClick={() => navigate('/cadastro')}>Quero Economizar</button>
-                    <div className="socials-icons">
-                        <InstagramIcon onClick={() => openUrl('https://instagram.com')} />
-                        <FacebookIcon onClick={() => openUrl('https://facebook.com')} />
-                        <WhatsappIcon onClick={() => openUrl('https://whatsapp.com')} />
+                    <LogoBranco className='logo' />
+                    <div className="links-container">
+                        <button onClick={() => navigate('/')}>Site</button>
+                        <button onClick={() => navigate('/')}>Simulador de Economia</button>
+                        <button onClick={() => navigate('/cadastro')}>Quero Economizar</button>
+                        <div className="socials-icons">
+                            <InstagramIcon onClick={() => openUrl('https://instagram.com')} />
+                            <FacebookIcon onClick={() => openUrl('https://facebook.com')} />
+                            <WhatsappIcon onClick={() => openUrl('https://whatsapp.com')} />
+                        </div>
                     </div>
+                    <div className="white-background"></div>
+                    <div className="top-blue-background"></div>
+                    <div className="bottom-blue-background"></div>
                 </div>
             </BackgroundContainer>
         </div>
