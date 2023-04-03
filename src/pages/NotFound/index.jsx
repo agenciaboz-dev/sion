cebe24525bdeefd12da2efbcc7dbe8257fecc9f4
ispadilha <israel.padilha@gmail.com>
2023-04-03 09:10:59 -0300

    update: error 404 not found page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import {ReactComponent as NotFoundIcon} from '../../images/404.svg'
 import { BackgroundContainer } from '../../components/BackgroundContainer';
+import {ReactComponent as LogoBranco} from '../../images/logo_branco.svg';
 import './style.scss';
 
 export const NotFound = () => {
@@ -10,15 +11,19 @@ export const NotFound = () => {
         <div className='NotFound-Page' >
             <BackgroundContainer>
                 <div className="main-container">
-                    <NotFoundIcon className='not-found-icon'/>
-                    <h1>Ops! página não encontrada</h1>
-                    <p>Desculpe, a página que você está procurando não existe. Se você acha que algo está quebrado, relate um problema.</p>
-                    <div className="buttons-container">
-                        <button className='not-found-back-button' onClick={() => navigate('/')}>Voltar</button>
+                    <LogoBranco className='logo'/>
+                    <div className="error404-container">
+                        <NotFoundIcon className='not-found-icon'/>
+                        <h1>Ops! página não encontrada</h1>
+                        <p>Desculpe, a página que você está procurando não existe. Se você acha que algo está quebrado, relate um problema.</p>
+                        <div className="buttons-container">
+                            <button className='not-found-back-button' onClick={() => navigate('/')}>Voltar</button>
+                            <button className='not-found-contact-button' onClick={() => navigate('#')}>Entre em contato</button>
+                        </div>
                     </div>
+                    <div className="white-background"></div>
+                    <div className="blue-background"></div>
                 </div>
-                <div className="white-background"></div>
-                <div className="blue-background"></div>
             </BackgroundContainer>
         </div>
     )
