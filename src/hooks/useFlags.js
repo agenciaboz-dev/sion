d79105e0d452d1258368763923dc71b1d2cc7052
nandobfer <nandobfer@yahoo.com.br>
2023-04-04 16:32:40 -0300

    feat: calculator formula

--- src/hooks/useFlags.js
+++ src/hooks/useFlags.js
@@ -1,24 +1,29 @@
+import { ReactComponent as GreenIcon } from '../images/simulator/green.svg'
+import { ReactComponent as YellowIcon } from '../images/simulator/yellow.svg'
+import { ReactComponent as RedIcon } from '../images/simulator/red.svg'
+import { ReactComponent as Red2Icon } from '../images/simulator/red2.svg'
+
 export const useFlags = () => {
     const flags = [
         {
-            name: 'Sem bandeira',
-            factor: 1,
-        },
-        {
             name: 'Verde',
-            factor: 0.82,
+            factor: 0.046,
+            icon: <GreenIcon />
         },
         {
             name: 'Amarela',
-            factor: 1.07,
+            factor: 0.06,
+            icon: <YellowIcon />
         },
         {
             name: 'Vermelha 1',
-            factor: 1.36,
+            factor: 0.076,
+            icon: <RedIcon />
         },
         {
             name: 'Vermelha 2',
-            factor: 1.79,
+            factor: 0.1,
+            icon: <Red2Icon />
         },
     ]
 
--- src/pages/Cadastro/Calculadora/index.jsx
+++ src/pages/Cadastro/Calculadora/index.jsx
@@ -12,21 +12,58 @@ import { useFlags } from '../../../hooks/useFlags';
 import { NavButtons } from '../NavButtons';
 import { useKWhMask } from '../../../hooks/useKWhMask';
 import { CurrentSupplier } from './CurrentSupplier';
+import CurrencyFormat from 'react-currency-format';
 
 export const Calculadora = ({  }) => {
 
+    const Flag = ({ flag }) => {
+        const Icon = () => flag.icon
+        const [economy, setEconomy] = useState(0)
+
+        useEffect(() => {
+            setEconomy(consumption * flag.factor)
+        }, [consumption])
+
+        return (
+            <div className="flag-container">
+                <Icon />
+                <hr />
+                <h3 style={{fontWeight: 'normal'}}>Anual</h3>
+                <CurrencyFormat
+                    value={economy*12} 
+                    displayType='text' 
+                    thousandSeparator='.'
+                    decimalSeparator=','
+                    decimalScale={2}
+                    fixedDecimalScale={true}
+                    prefix={'R$ '}
+                    style={{fontSize: '5vw', fontWeight: 'bold', color: COLORS.primary}}
+                />
+                <p>Mensal</p>
+                <CurrencyFormat
+                    value={economy} 
+                    displayType='text' 
+                    thousandSeparator='.'
+                    decimalSeparator=','
+                    decimalScale={2}
+                    fixedDecimalScale={true}
+                    prefix={'R$ '}
+                    style={{color: COLORS.primary}}
+                />
+
+            </div>
+        )
+    }
+
     const client = useClient()
-    const navigate = useNavigate()
-    const discountRef = useRef(null)
-    const profitRef = useRef(null)
     const percentMask = usePercentMask()
-    const currencyMask = useCurrencyMask()
     const kwhMask = useKWhMask()
     const flags = useFlags()
 
     const formRef = useRef(null)
 
     const [spent, setSpent] = useState(client.value?.spent || '2000')
+    const [consumption, setConsumption] = useState(0)
 
     const nextStage = () => {
         client.setValue({...client.value, spent})
@@ -35,9 +72,9 @@ export const Calculadora = ({  }) => {
     }
 
     useEffect(() => {
-        
-    }, [])
-    
+        setConsumption(parseInt(spent.replace(/\D/, '')))
+    }, [spent])
+
     return (
         <div className='Calculadora-Component' >
             <h1>Projeção de economia</h1>
@@ -45,6 +82,10 @@ export const Calculadora = ({  }) => {
             <CurrentSupplier formRef={formRef} />
 
             <InputField title={'Consumo mensal médio'} mask={kwhMask} id={'spent'} value={spent} handleChange={event => setSpent(event.target.value)} not_required />
+
+            <div className="flags-container">
+                {flags.map(flag => <Flag flag={flag} />)}
+            </div>
 
             <NavButtons nextOnly nextStage={nextStage} />
         </div>
