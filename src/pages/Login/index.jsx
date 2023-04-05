5727600f99f849a539b08e3e4fa51ff0a79801ab
nandobfer <nandobfer@yahoo.com.br>
2023-04-05 18:36:52 -0300

    update: changed logo and added client button if id param

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,15 +1,15 @@
-import { Checkbox, FormControlLabel } from '@mui/material';
+import { Button, Checkbox, FormControlLabel } from '@mui/material';
 import { Form, Formik } from 'formik';
 import { useEffect, useState } from 'react';
 import { api } from '../../api';
 import { Footer } from '../../components/Footer';
 import { InputField } from '../../components/InputField';
 import { useLocalStorage } from '../../hooks/useLocalStorage';
-import { ReactComponent as LogoCromado } from '../../images/logo_cromado_svg.svg'
+import { ReactComponent as LogoCromado } from '../../images/logo_bonita.svg'
 import CircularProgress from '@mui/material/CircularProgress';
 import './style.scss';
 import { useUser } from '../../hooks/useUser';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { MuiLoading } from '../../components/MuiLoading';
 import { Header } from '../../components/Header';
 
@@ -18,6 +18,7 @@ export const Login = () => {
     const storage = useLocalStorage()
     const [user, setUser] = useUser()
     const navigate = useNavigate()
+    const params = useParams()
 
     const [remind, setRemind] = useState(storage.get('remindme'))
     const [loading, setLoading] = useState(false)
@@ -34,7 +35,7 @@ export const Login = () => {
                 storage.set('user_sion', remind ? usuario : null)
                 setUser(usuario)
 
-                navigate('/cadastro')
+                navigate(params.id ? '/sign/'+params.id : '/cadastro')
             } else {
                 setError(true)
             }
@@ -51,7 +52,12 @@ export const Login = () => {
     }, [remind])
 
     useEffect(() => {
-        if (user?.username) navigate('/cadastro')
+        if (params.id) {
+            if (user?.username) navigate('/sign/'+params.id)
+        } else {
+            if (user?.username) navigate('/cadastro')
+        }
+        
     }, [])
     
     return (
@@ -62,6 +68,22 @@ export const Login = () => {
                 <Formik initialValues={{user: '', password: ''}} onSubmit={values => tryLogin(values)}>
                     {({handleChange, values, submitForm, errors}) => (
                         <Form>
+                             
+                             { params.id &&
+                                <div className="client-button">
+                                        <Button variant='contained' fullWidth onClick={() => navigate('/sign/'+params.id)} >Sou Cliente</Button>
+                                    <div className='or'>
+                                        <div>
+                                            <hr />
+                                        </div>
+                                        <p>OU</p>
+                                        <div>
+                                            <hr />
+                                        </div>
+                                    </div>
+                                   
+                                </div>
+                            }
                             <div className="user-input-container">
                                 <label>Nome de usuário ou e-mail</label>
                                 <InputField title={''} id={'user'} handleChange={handleChange} value={values.user} error={Boolean(errors.user)} errorText={errors.user} />
@@ -81,6 +103,7 @@ export const Login = () => {
                                     <button type='submit'>{loading ? <MuiLoading /> : 'Entrar'}</button>
                                 </div>
                             </div>
+                            
                         </Form>
                     )}
                 </Formik>
