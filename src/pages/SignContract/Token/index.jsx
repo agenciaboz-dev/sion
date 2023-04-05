80c4ea4dbeb98cbf3f3dbd78b7fa4d5d026c53d8
nandobfer <nandobfer@yahoo.com.br>
2023-04-05 18:37:52 -0300

    feat: added sign api call

--- src/pages/SignContract/Token/index.jsx
+++ src/pages/SignContract/Token/index.jsx
@@ -1,6 +1,10 @@
 import ReactCodeInput from 'react-verification-code-input';
+import { api } from '../../../api';
+import { useUser } from '../../../hooks/useUser';
 
 export const Token = ({ setOpenSnackbar, setError, setStage, contract }) => {
+
+    const [user, setUser] = useUser()
 
     const handleSubmit = values => {
         console.log(values)
@@ -8,7 +12,19 @@ export const Token = ({ setOpenSnackbar, setError, setStage, contract }) => {
 
     const verifyToken = (value) => {
         if (value == contract.token) {
+            const data = { 
+                id: contract.id, 
+                name: user?.name || contract.name,
+                email: user?.email || contract.email,
+                cpf: user?.cpf || contract.cpf
+            }
+            
+            api.post('/contract/sign', data)
+            .then(response => console.log(response.data))
+            .catch(error => console.error(error))
+
             setStage(3)
+
         } else {
             setOpenSnackbar(true)
             setError('Token inválido')
