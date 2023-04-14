66cae789329d8c83fd3bd30f3631d4c9a65b1648
ispadilha <israel.padilha@gmail.com>
2023-04-14 10:28:06 -0300

    update: customers section design

--- src/components/WhoGets/index.jsx
+++ src/components/WhoGets/index.jsx
@@ -1,12 +1,14 @@
 import './style.scss';
 import {ReactComponent as Arrow} from '../../images/arrow.svg'
 import { CustomerPic } from './CustomerPic';
-import { useCustomers } from '../../hooks/useCustomers';
+import { useCustomersTop } from '../../hooks/useCustomersTop';
+import { useCustomersBottom } from '../../hooks/useCustomersBottom';
 import useMeasure from 'react-use-measure';
 
 export const WhoGets = () => {
     
-    const customers = useCustomers()
+    const customersTop = useCustomersTop()
+    const customersBottom = useCustomersBottom()
     const [ref, {height}] = useMeasure()
 
     return (
@@ -16,15 +18,24 @@ export const WhoGets = () => {
 
             <h1>Quem recebe nossa energia</h1>
             <div className="main-container">
-                <Arrow style={{cursor: 'pointer', width: '3vw'}} />
+                {/* <Arrow style={{cursor: 'pointer', width: '3vw'}} /> */}
                 <div className="pictures">
-                    {customers.map(customer => {
-                        return (
-                            <CustomerPic key={customers.indexOf(customer)} customer={customer} />
-                        )
-                    })}
+                    <div className="customers-top">
+                        {customersTop.map(customer => {
+                            return (
+                                <CustomerPic key={customersTop.indexOf(customer)} customer={customer} />
+                            )
+                        })}
+                    </div>
+                    <div className="customers-bottom">
+                        {customersBottom.map(customer => {
+                            return (
+                                <CustomerPic key={customersBottom.indexOf(customer)} customer={customer} />
+                            )
+                        })}
+                    </div>
                 </div>
-                <Arrow style={{transform: 'scale(-1, 1)', cursor: 'pointer', filter: 'invert(94%) sepia(100%) saturate(1%) hue-rotate(87deg) brightness(104%) contrast(102%)', width: '3vw'}} />
+                {/* <Arrow style={{transform: 'scale(-1, 1)', cursor: 'pointer', filter: 'invert(94%) sepia(100%) saturate(1%) hue-rotate(87deg) brightness(104%) contrast(102%)', width: '3vw'}} /> */}
             </div>
         </div>
     )
