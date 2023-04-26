8ea9d7d831718438656ef77d33117ee362605d5a
nandobfer <nandobfer@yahoo.com.br>
2023-04-26 17:42:01 -0300

    feat: added credentials to financial

--- src/pages/Cadastro/ContatoFinanceiro/index.jsx
+++ src/pages/Cadastro/ContatoFinanceiro/index.jsx
@@ -15,50 +15,79 @@ export const ContatoFinanceiro = ({  }) => {
     const [loading, setLoading] = useState(false)
 
     const initialValues = {
-        name: '',
-        phone: '',
-        email: ''
-    }
+		name: '',
+		phone: '',
+		email: '',
+		login: '',
+		password: '',
+	}
 
-    const goBack = (event) => {
-        event.preventDefault()
-        navigate('/cadastro/contrato')
-    }
+	const goBack = (event) => {
+		event.preventDefault()
+		navigate('/cadastro/contrato')
+	}
 
-    const nextStage = (values) => {
-        client.setValue({...client.value, financial: values})
-        setLoading(true)
+	const nextStage = (values) => {
+		client.setValue({ ...client.value, financial: values })
+		setLoading(true)
 
-        api.post('/contract/financial', {...values, id: client.value.id})
-        .then(response => console.log(response.data))
-        .catch(error => console.error(error))
-        .finally(() => {
-            setLoading(false)
-            client.setValue(null)
-            navigate('/')
-        })
-    }
-    
-    return (
-        <div className='ContatoFinanceiro-Component' >
-            <h2>Cadastro Financeiro</h2>
-            <Formik initialValues={initialValues} onSubmit={nextStage} >
-                {({values, handleChange, submitForm}) => 
-                <Form onKeyDown={(event) => {
-                    if (event.key === 'Enter') {
-                        event.preventDefault();
-                        submitForm();
-                    }
-                    }}>
-                    <InputField title={'Responsável Financeiro'} id='name' value={values.name} handleChange={handleChange} />
-                    <InputField title={'Telefone Financeiro'} inputMode={'tel'} mask={["(", /\d/, /\d/, ")", " ", /\d/, /\d/, /\d/, /\d/, /\d/, "-", /\d/, /\d/, /\d/, /\d/]} id='phone' value={values.phone} handleChange={handleChange} />
-                    <InputField title={'E-mail do responsável financeiro'} id='email' value={values.email} handleChange={handleChange} />
-                    <NavButtons goBack={(event) => goBack(event)} nextStage={(event) => {
-                        event.preventDefault()
-                        submitForm()
-                    }} children={loading ? <MuiLoading size={'5vw'} /> : <>Finalizar</>} />
-                </Form>}
-            </Formik>
-        </div>
-    )
+		api.post('/contract/financial', { ...values, id: client.value.id })
+			.then((response) => console.log(response.data))
+			.catch((error) => console.error(error))
+			.finally(() => {
+				setLoading(false)
+				client.setValue(null)
+				navigate('/')
+			})
+	}
+
+	return (
+		<div className="ContatoFinanceiro-Component">
+			<h2>Cadastro Financeiro</h2>
+			<Formik initialValues={initialValues} onSubmit={nextStage}>
+				{({ values, handleChange, submitForm }) => (
+					<Form
+						onKeyDown={(event) => {
+							if (event.key === 'Enter') {
+								event.preventDefault()
+								submitForm()
+							}
+						}}
+					>
+						<InputField
+							title={'Responsável Financeiro'}
+							id="name"
+							value={values.name}
+							handleChange={handleChange}
+						/>
+						<InputField
+							title={'Telefone Financeiro'}
+							inputMode={'tel'}
+							mask={['(', /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
+							id="phone"
+							value={values.phone}
+							handleChange={handleChange}
+						/>
+						<InputField
+							title={'E-mail do responsável financeiro'}
+							id="email"
+							value={values.email}
+							handleChange={handleChange}
+						/>
+						<h2>Credenciais</h2>
+						<InputField title={'Login'} id="login" value={values.login} handleChange={handleChange} />
+						<InputField title={'Senha'} id="password" value={values.password} handleChange={handleChange} />
+						<NavButtons
+							goBack={(event) => goBack(event)}
+							nextStage={(event) => {
+								event.preventDefault()
+								submitForm()
+							}}
+							children={loading ? <MuiLoading size={'5vw'} /> : <>Finalizar</>}
+						/>
+					</Form>
+				)}
+			</Formik>
+		</div>
+	)
 }
