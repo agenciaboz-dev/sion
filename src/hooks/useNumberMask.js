5bb66bd067716f963fe0da96383e13e6b9068acd
nandobfer <nandobfer@yahoo.com.br>
2023-04-26 17:42:25 -0300

    update: added number mask to calculator

---
+++ src/hooks/useNumberMask.js
@@ -0,0 +1,17 @@
+import { createNumberMask } from 'text-mask-addons'
+
+export const useNumberMask = () => {
+	const numberMask = createNumberMask({
+		prefix: '',
+		suffix: '',
+		thousandsSeparatorSymbol: '.',
+		decimalSymbol: ',',
+		allowDecimal: true,
+		decimalLimit: 2,
+		integerLimit: 9,
+		allowNegative: false,
+		allowLeadingZeroes: false,
+	})
+
+	return numberMask
+}
--- src/pages/Cadastro/Calculadora/index.jsx
+++ src/pages/Cadastro/Calculadora/index.jsx
@@ -9,101 +9,110 @@ import { CurrentSupplier } from './CurrentSupplier';
 import CurrencyFormat from 'react-currency-format';
 import { useUser } from '../../../hooks/useUser';
 import { useNavigate } from 'react-router-dom';
+import { useNumberMask } from '../../../hooks/useNumberMask'
 
-export const Calculadora = ({  }) => {
+export const Calculadora = ({}) => {
+	const Flag = ({ flag }) => {
+		const Icon = () => flag.icon
+		const Tone = () => flag.name
+		const [economy, setEconomy] = useState(0)
 
-    const Flag = ({ flag }) => {
-        const Icon = () => flag.icon
-        const Tone = () => flag.name
-        const [economy, setEconomy] = useState(0)
+		useEffect(() => {
+			setEconomy(consumption * flag.factor)
+			console.log(consumption)
+		}, [consumption])
 
-        useEffect(() => {
-            setEconomy(consumption * flag.factor)
-            console.log(consumption)
-        }, [consumption])
+		return (
+			<div className="flag-container">
+				<div className="flag-tone">
+					<Icon />
+					<Tone />
+				</div>
+				<hr />
+				<div className="yearly">
+					<h3 style={{ fontWeight: 'normal' }}>Anual</h3>
+					<CurrencyFormat
+						value={economy * 12}
+						displayType="text"
+						thousandSeparator="."
+						decimalSeparator=","
+						decimalScale={2}
+						fixedDecimalScale={true}
+						prefix={'R$ '}
+						style={{ fontSize: '5vw', fontWeight: 'bold', color: COLORS.primary }}
+					/>
+				</div>
+				<hr />
+				<div className="monthly">
+					<p>Mensal</p>
+					<CurrencyFormat
+						value={economy}
+						displayType="text"
+						thousandSeparator="."
+						decimalSeparator=","
+						decimalScale={2}
+						fixedDecimalScale={true}
+						prefix={'R$ '}
+						style={{ color: COLORS.primary }}
+					/>
+				</div>
+			</div>
+		)
+	}
 
-        return (
-            <div className="flag-container">
-                <div className="flag-tone">
-                    <Icon />
-                    <Tone />
-                </div>
-                <hr />
-                <div className="yearly">
-                    <h3 style={{fontWeight: 'normal'}}>Anual</h3>
-                    <CurrencyFormat
-                        value={economy*12}
-                        displayType='text'
-                        thousandSeparator='.'
-                        decimalSeparator=','
-                        decimalScale={2}
-                        fixedDecimalScale={true}
-                        prefix={'R$ '}
-                        style={{fontSize: '5vw', fontWeight: 'bold', color: COLORS.primary}}
-                    />
-                </div>
-                <hr />
-                <div className="monthly">
-                <p>Mensal</p>
-                    <CurrencyFormat
-                        value={economy}
-                        displayType='text'
-                        thousandSeparator='.'
-                        decimalSeparator=','
-                        decimalScale={2}
-                        fixedDecimalScale={true}
-                        prefix={'R$ '}
-                        style={{color: COLORS.primary}}
-                    />
-                </div>
+	const client = useClient()
+	const flags = useFlags()
+	const [user, setUser] = useUser()
+	const navigate = useNavigate()
+	const numberMask = useNumberMask()
+	const formRef = useRef(null)
 
-            </div>
-        )
-    }
+	const [validUnit, setValidUnit] = useState(false)
+	const [spent, setSpent] = useState(client.value?.spent || '')
+	const [consumption, setConsumption] = useState(0)
 
-    const client = useClient()
-    const flags = useFlags()
-    const [user, setUser] = useUser()
-    const navigate = useNavigate()
-    
-    const formRef = useRef(null)
-    
-    const [validUnit, setValidUnit] = useState(false)
-    const [spent, setSpent] = useState(client.value?.spent || '')
-    const [consumption, setConsumption] = useState(0)
+	const nextStage = () => {
+		client.setValue({ ...client.value, spent })
 
-    const nextStage = () => {
-        client.setValue({...client.value, spent})
+		formRef.current.submitForm()
+	}
 
-        formRef.current.submitForm()
-    }
+	useEffect(() => {
+		if (spent) {
+			setConsumption(Number(spent.replace(/\D/, '')))
+		} else {
+			setConsumption(0)
+		}
+	}, [spent])
 
-    useEffect(() => {
-        if (spent) {
-            setConsumption(Number(spent.replace(/\D/, '')))
-        } else {
-            setConsumption(0)
-        }
-        
-    }, [spent])
+	useEffect(() => {
+		if (!user) navigate('/login')
+	}, [])
 
-    useEffect(() => {
-        if (!user) navigate('/login')
-    }, [])
+	return (
+		<div className="Calculadora-Component">
+			<h1>Projeção de economia</h1>
 
-    return (
-        <div className='Calculadora-Component' >
-            <h1>Projeção de economia</h1>
+			<CurrentSupplier formRef={formRef} setValidUnit={setValidUnit} />
 
-            <CurrentSupplier formRef={formRef} setValidUnit={setValidUnit} />
+			<InputField
+				title={'Consumo mensal médio'}
+				endAdornment={<p className="kwh">kWh</p>}
+				inputMode="numeric"
+				id={'spent'}
+				value={spent}
+				handleChange={(event) => setSpent(event.target.value)}
+				not_required
+				mask={numberMask}
+			/>
 
-            <InputField title={'Consumo mensal médio'} endAdornment={<p className='kwh'>kWh</p>} id={'spent'} value={spent} handleChange={event => setSpent(event.target.value)} not_required />
+			<div className="flags-container">
+				{flags.map((flag) => (
+					<Flag key={flag.id} flag={flag} />
+				))}
+			</div>
 
-            <div className="flags-container">
-                {flags.map(flag => <Flag key={flag.id} flag={flag} />)}
-            </div>
-
-            <NavButtons nextOnly nextStage={nextStage} disabledNext={!validUnit} />
-        </div>
-    )
+			<NavButtons nextOnly nextStage={nextStage} disabledNext={!validUnit} />
+		</div>
+	)
 }
