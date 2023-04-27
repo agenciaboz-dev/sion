5362a43ce72764f48591af8da7762aaee11f9a53
ispadilha <israel.padilha@gmail.com>
2023-04-27 11:39:37 -0300

    Changed how-it-works section title line-height

--- src/components/HowWorks/index.jsx
+++ src/components/HowWorks/index.jsx
@@ -19,7 +19,7 @@ export const HowWorks = ({ main_container_height, advert_height }) => {
 
             </div>
             <div className="blue-background"></div>
-            <div className="white-background" style={{height: isMobile? height : height * 1.15}}></div>
+            <div className="white-background" style={{height: isMobile? height : height * 1.25}}></div>
             <div className="how-works-containers">
                 <div className="details-container">
                     <h1>Uma maneira simples de você economizar com a energia do seu negócio</h1>
