637fe38b6464d265314d9b8898d786cf947c6eb3
ispadilha <israel.padilha@gmail.com>
2023-04-27 11:11:55 -0300

    Update: who-gets section grid

--- src/components/WhoGets/index.jsx
+++ src/components/WhoGets/index.jsx
@@ -1,14 +1,8 @@
 import './style.scss';
-import {ReactComponent as Arrow} from '../../images/arrow.svg'
-import { CustomerPic } from './CustomerPic';
-import { useCustomersTop } from '../../hooks/useCustomersTop';
-import { useCustomersBottom } from '../../hooks/useCustomersBottom';
 import useMeasure from 'react-use-measure';
 
 export const WhoGets = () => {
     
-    const customersTop = useCustomersTop()
-    const customersBottom = useCustomersBottom()
     const [ref, {height}] = useMeasure()
 
     return (
@@ -16,26 +10,36 @@ export const WhoGets = () => {
             <div className="blue-background"></div>
             <div className="white-background"></div>
 
-            <h1>Quem recebe nossa energia</h1>
             <div className="main-container">
-                {/* <Arrow style={{cursor: 'pointer', width: '3vw'}} /> */}
-                <div className="pictures">
-                    <div className="customers-top">
-                        {customersTop.map(customer => {
-                            return (
-                                <CustomerPic key={customersTop.indexOf(customer)} customer={customer} />
-                            )
-                        })}
+                <div className="customers-images">
+                    <div className="who">
+                        <h1>Quem recebe nossa energia</h1>
                     </div>
-                    <div className="customers-bottom">
-                        {customersBottom.map(customer => {
-                            return (
-                                <CustomerPic key={customersBottom.indexOf(customer)} customer={customer} />
-                            )
-                        })}
+                    <div className="c-img">
+                        <img src="/images/customers/confiance.webp" alt="" />
+                    </div>
+                    <div className="c-img">
+                        <img src="/images/customers/toyota.webp" alt="" />
+                    </div>
+                    <div className="c-img">
+                        <img src="/images/customers/elasto.webp" alt="" />
+                    </div>
+                    <div className="c-img">
+                        <img src="/images/customers/mcdonalds.webp" alt="" />
+                    </div>
+                    <div className="c-img">
+                        <img src="/images/customers/baraquias.webp" alt="" />
+                    </div>
+                    <div className="c-img">
+                        <img src="/images/customers/varandas.webp" alt="" />
+                    </div>
+                    <div className="c-img">                        
+                        <img src="/images/customers/rededutra.webp" alt="" />
+                    </div>
+                    <div className="c-img">
+                        <img src="/images/customers/ipiranga.webp" alt="" />
                     </div>
                 </div>
-                {/* <Arrow style={{transform: 'scale(-1, 1)', cursor: 'pointer', filter: 'invert(94%) sepia(100%) saturate(1%) hue-rotate(87deg) brightness(104%) contrast(102%)', width: '3vw'}} /> */}
             </div>
         </div>
     )
