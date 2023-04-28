206258d7fe8f68ebab9d31cdc2ac8a6207b440ce
nandobfer <nandobfer@yahoo.com.br>
2023-04-28 10:03:33 -0300

    update: removed decimal from numbermask

--- src/hooks/useNumberMask.js
+++ src/hooks/useNumberMask.js
@@ -2,16 +2,16 @@ import { createNumberMask } from 'text-mask-addons'
 
 export const useNumberMask = () => {
 	const numberMask = createNumberMask({
-		prefix: '',
-		suffix: '',
-		thousandsSeparatorSymbol: '.',
-		decimalSymbol: ',',
-		allowDecimal: true,
-		decimalLimit: 2,
-		integerLimit: 9,
-		allowNegative: false,
-		allowLeadingZeroes: false,
-	})
+    prefix: "",
+    suffix: "",
+    thousandsSeparatorSymbol: ".",
+    decimalSymbol: ",",
+    allowDecimal: false,
+    decimalLimit: 2,
+    integerLimit: 9,
+    allowNegative: false,
+    allowLeadingZeroes: false,
+  })
 
 	return numberMask
 }
