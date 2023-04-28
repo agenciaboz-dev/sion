80b2a733f38d0cfe09216ad3109ed525d8e56a19
nandobfer <nandobfer@yahoo.com.br>
2023-04-28 09:13:26 -0300

    fix: NaN on calculator

--- src/components/Simulator/index.jsx
+++ src/components/Simulator/index.jsx
@@ -12,101 +12,124 @@ import { TextField } from '@mui/material';
 import { useFlags } from '../../hooks/useFlags';
 import CurrencyFormat from 'react-currency-format';
 import COLORS from '../../sass/_colors.scss'
+import { useNumberMask } from "../../hooks/useNumberMask"
 
 export const Simulator = () => {
-
-    const Flag = ({ flag }) => {
-        const Icon = () => flag.icon
-        const [economy, setEconomy] = useState(0)
-
-        useEffect(() => {
-            setEconomy(consumption*flag.factor)
-        }, [consumption])
-        return (
-            <div className="flag-container">
-                <Icon />
-                <hr />
-                <h3>Anual</h3>
-                <CurrencyFormat
-                    value={economy*12} 
-                    displayType='text' 
-                    thousandSeparator='.'
-                    decimalSeparator=','
-                    decimalScale={2}
-                    fixedDecimalScale={true}
-                    prefix={'R$ '}
-                    style={{fontSize: '1vw', fontWeight: 'bold', color: COLORS.primary}}
-                />
-                <hr />
-                <p>Mensal</p>
-                <CurrencyFormat
-                    value={economy*12} 
-                    displayType='text' 
-                    thousandSeparator='.'
-                    decimalSeparator=','
-                    decimalScale={2}
-                    fixedDecimalScale={true}
-                    prefix={'R$ '}
-                    style={{fontSize: '0.7vw', color: COLORS.primary}}
-                />
-            </div>
-        )
-    }
-
-    const [spent, setSpent] = useState('2000')
-    const [consumption, setConsumption] = useState(0)
-
-    const navigate = useNavigate()
-    const flags = useFlags()
-
-    const kwhMask = createNumberMask({
-        prefix: '',
-        suffix: ' kWh',
-        includeThousandsSeparator: true,
-        thousandsSeparatorSymbol: '.',
-        allowDecimal: true,
-        decimalSymbol: ',',
-        decimalLimit: 2,
-        allowNegative: false,
-        allowLeadingZeroes: false,
-    })
-
-    const goToSignUp = () => {
-        window.scrollTo(0, 200);
-        navigate('/cadastro')
-    }
+  const Flag = ({ flag }) => {
+    const Icon = () => flag.icon
+    const [economy, setEconomy] = useState(0)
 
     useEffect(() => {
-        setConsumption(parseInt(spent.replace(/\D/, '')))
-    }, [spent])
-
+      setEconomy(consumption * flag.factor)
+    }, [consumption])
     return (
-        <div className='Simulator-Component' id='simulator' >
-            <div className="white-container">
-                <MaskedInput 
-                    mask={kwhMask}
-                    value={spent} onChange={event => setSpent(event.target.value)}
-                    render={(ref, props) => (
-                        <TextField
-                            inputRef={ref}
-                            {...props}
-                            label='Consumo mensal'
-                            inputProps={{inputMode: 'numeric'}}
-                        />
-                    )}
-                />
-                <div className="flags-container">
-                    {flags.map(flag => <Flag flag={flag} />)}
-                </div>
-            </div>
-            <div className="blue-container">
-                <div className="benefits-container">
-                    <Benefits icon={() => <DollarIcon />} title={'Economia'} text='Receba seus créditos de energia e economize em até 15% na sua fatura' />
-                    <Benefits icon={() => <WalletIcon />} title={'Sem investimento'} text='Sem necessidade de alteração física no seu negócio ou investimento' />
-                    <Benefits icon={() => <SunIcon />} title={'Energia renovável'} text='Os créditos de energia são gerados por fontes renováveis' />
-                </div>
-                <button onClick={() => goToSignUp()} className='simulator-signup-button'>Quero economizar!</button>
-            </div>
-        </div>
+      <div className="flag-container">
+        <Icon />
+        <hr />
+        <h3>Anual</h3>
+        <CurrencyFormat
+          value={economy * 12}
+          displayType="text"
+          thousandSeparator="."
+          decimalSeparator=","
+          decimalScale={2}
+          fixedDecimalScale={true}
+          prefix={"R$ "}
+          style={{ fontSize: "1vw", fontWeight: "bold", color: COLORS.primary }}
+        />
+        <hr />
+        <p>Mensal</p>
+        <CurrencyFormat
+          value={economy * 12}
+          displayType="text"
+          thousandSeparator="."
+          decimalSeparator=","
+          decimalScale={2}
+          fixedDecimalScale={true}
+          prefix={"R$ "}
+          style={{ fontSize: "0.7vw", color: COLORS.primary }}
+        />
+      </div>
     )
+  }
+
+  const [spent, setSpent] = useState("")
+  const [consumption, setConsumption] = useState(0)
+
+  const navigate = useNavigate()
+  const flags = useFlags()
+  const numberMask = useNumberMask()
+
+  const kwhMask = createNumberMask({
+    prefix: "",
+    suffix: " kWh",
+    includeThousandsSeparator: true,
+    thousandsSeparatorSymbol: ".",
+    allowDecimal: true,
+    decimalSymbol: ",",
+    decimalLimit: 2,
+    allowNegative: false,
+    allowLeadingZeroes: false,
+  })
+
+  const goToSignUp = () => {
+    window.scrollTo(0, 200)
+    navigate("/cadastro")
+  }
+
+  useEffect(() => {
+    if (spent) {
+      setConsumption(parseInt(spent.replace(/\D/, "")))
+    } else {
+      setConsumption(0)
+    }
+  }, [spent])
+
+  return (
+    <div className="Simulator-Component" id="simulator">
+      <div className="white-container">
+        <MaskedInput
+          mask={kwhMask}
+          value={spent}
+          onChange={(event) => setSpent(event.target.value)}
+          render={(ref, props) => (
+            <TextField
+              inputRef={ref}
+              {...props}
+              label="Consumo mensal"
+              inputProps={{ inputMode: "numeric" }}
+              mask={numberMask}
+            />
+          )}
+        />
+        <div className="flags-container">
+          {flags.map((flag) => (
+            <Flag flag={flag} />
+          ))}
+        </div>
+      </div>
+      <div className="blue-container">
+        <div className="benefits-container">
+          <Benefits
+            icon={() => <DollarIcon />}
+            title={"Economia"}
+            text="Receba seus créditos de energia e economize em até 15% na sua fatura"
+          />
+          <Benefits
+            icon={() => <WalletIcon />}
+            title={"Sem investimento"}
+            text="Sem necessidade de alteração física no seu negócio ou investimento"
+          />
+          <Benefits
+            icon={() => <SunIcon />}
+            title={"Energia renovável"}
+            text="Os créditos de energia são gerados por fontes renováveis"
+          />
+        </div>
+        <button onClick={() => goToSignUp()} className="simulator-signup-button">
+          Quero economizar!
+        </button>
+      </div>
+    </div>
+  )
 }
