63908c436577a99b8edaa00b6406eec6713ca8ab
nandobfer <nandobfer@yahoo.com.br>
2023-05-03 18:38:46 -0300

    update: pessoa style desktop

--- src/pages/Cadastro/PessoaChooser/index.jsx
+++ src/pages/Cadastro/PessoaChooser/index.jsx
@@ -1,28 +1,38 @@
 import { useEffect } from "react"
 import { useState } from "react"
-import { useMediaQuery } from "react-responsive"
+import { useMediaQuery } from "@mui/material"
 import { useNavigate } from "react-router-dom"
 import { useClient } from "../../../hooks/useClient"
+import { useColors } from "../../../hooks/useColors"
 import { useStage } from "../../../hooks/useStage"
-import {ReactComponent as ChoseIcon} from '../../../images/check.svg'
-import {ReactComponent as ForwardArrowBlue} from '../../../images/forward_arrow_small_blue.svg';
+import { ReactComponent as ChoseIcon } from "../../../images/check.svg"
+import { ReactComponent as ForwardArrowBlue } from "../../../images/forward_arrow_small_blue.svg"
+import { ReactComponent as ForwardArrow } from "../../../images/forward_arrow_small.svg"
 
 export const PessoaChooser = ({ pessoa, setPessoa }) => {
-        
     const Pessoa = ({ name, value, description }) => {
         const [clicked, setClicked] = useState(false)
+        const [backgroundColor, setBackgroundColor] = useState("white")
 
         const nextStage = () => {
-            client.setValue({...client.value, pessoa: value})
+            client.setValue({ ...client.value, pessoa: value })
             setPessoa(value)
-            setTimeout(() => {
-                navigate('/cadastro/formulario')
-            }, 500)
+            // setTimeout(() => {
+            //     navigate("/cadastro/formulario")
+            // }, 500)
         }
+
+        useEffect(() => {
+            console.log(backgroundColor)
+        }, [backgroundColor])
 
         useEffect(() => {
             if (pessoa == value) {
                 setClicked(true)
+                setBackgroundColor(colors.primary)
+            } else {
+                setClicked(false)
+                setBackgroundColor(isMobile ? "white" : colors.primary_light)
             }
         }, [pessoa])
 
@@ -32,8 +42,14 @@ export const PessoaChooser = ({ pessoa, setPessoa }) => {
                     <h1>{name}</h1>
                     <p>{description}</p>
                 </div>
-                <div className="chose-container" style={{backgroundColor: clicked ? '#384974' : 'white'}}>
-                    {clicked ? <ChoseIcon style={isMobile && {height:'18vw', width: '18vw'}} /> : <div className='forward-arrow-container'><ForwardArrowBlue /></div> }
+                <div className="chose-container" style={{ backgroundColor: backgroundColor }}>
+                    {clicked ? (
+                        <ChoseIcon style={isMobile ? { height: "18vw", width: "18vw" } : { height: "4vw", width: "4vw" }} />
+                    ) : (
+                        <div className="forward-arrow-container">
+                            {isMobile ? <ForwardArrowBlue /> : <ForwardArrow style={{ height: "4vw", width: "4vw" }} />}
+                        </div>
+                    )}
                 </div>
             </div>
         )
@@ -41,16 +57,28 @@ export const PessoaChooser = ({ pessoa, setPessoa }) => {
 
     const navigate = useNavigate()
     const client = useClient()
-    const isMobile = useMediaQuery({maxWidth: 600})
+    const isMobile = useMediaQuery("(orientation: portrait)")
+    const colors = useColors()
     const { setStage, setBar } = useStage()
 
-    useEffect(() => {
-    }, [])
+    useEffect(() => {}, [])
 
     return (
         <div className="pessoa-wrapper">
-            <Pessoa name='Pessoa Física' value='fisica' description={''} />
-            <Pessoa name='Pessoa Jurídica' value='juridica' description={''} />
+            <Pessoa
+                name="Pessoa Física"
+                value="fisica"
+                description={
+                    "Uma pessoa física pode ser identificada por meio de seus documentos pessoais, como CPF (Cadastro de Pessoa Física), RG (Registro Geral), carteira de motorista, entre outros. Ela é capaz de realizar transações financeiras, assinar contratos, adquirir bens e serviços, entre outras atividades."
+                }
+            />
+            <Pessoa
+                name="Pessoa Jurídica"
+                value="juridica"
+                description={
+                    "Uma pessoa jurídica pode ser identificada por meio do seu CNPJ (Cadastro Nacional de Pessoa Jurídica), que é um registro único que identifica a empresa perante os órgãos públicos e as instituições financeiras. O CNPJ é composto por 14 dígitos e pode ser consultado por qualquer pessoa por meio do site da Receita Federal."
+                }
+            />
         </div>
     )
 }
