f2ea1a5dbd08ebc0253e8ec7bf7f7a7397cb991f
ispadilha <israel.padilha@gmail.com>
2023-05-10 10:09:07 -0300

    Changed whatsapp-button icon

--- src/components/WhatsappButton/index.jsx
+++ src/components/WhatsappButton/index.jsx
@@ -1,11 +1,14 @@
 import './style.scss';
-import {ReactComponent as WhatsappIcon} from '../../images/whatsapp_green_icon.svg'
+// import {ReactComponent as WhatsappIcon} from '../../images/whatsapp_green_icon.svg'
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 export const WhatsappButton = () => {
 
     return (
       <div className="WhatsappButton-Component" onClick={() => (window.location.href = "https://wa.me/554130283782")}>
-        <WhatsappIcon />
+        <WhatsAppIcon sx={
+          'background: #00B314; color: white; height: 4.25vw; width: 4.25vw; padding: 0.75vw; border-radius: 50%'
+          }/>
       </div>
     )
 
