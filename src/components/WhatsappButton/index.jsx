60791b5bf228b59e4e869a32b666775d7c3d347d
ispadilha <israel.padilha@gmail.com>
2023-05-11 09:29:22 -0300

    fix: adjusted weni and whatsapp buttons

--- src/components/WhatsappButton/index.jsx
+++ src/components/WhatsappButton/index.jsx
@@ -1,15 +1,16 @@
 import './style.scss';
 // import {ReactComponent as WhatsappIcon} from '../../images/whatsapp_green_icon.svg'
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import { useMediaQuery } from 'react-responsive';
 
 export const WhatsappButton = () => {
+  const isMobile = useMediaQuery({maxWidth: 600})
 
-    return (
-      <div className="WhatsappButton-Component" onClick={() => (window.location.href = "https://wa.me/554130283782")}>
-        <WhatsAppIcon sx={
-          'background: #00B314; color: white; height: 4.25vw; width: 4.25vw; padding: 0.75vw; border-radius: 50%'
-          }/>
-      </div>
-    )
+  return (
+    <div className="WhatsappButton-Component" onClick={() => (window.location.href = "https://wa.me/554130283782")}>
+      <WhatsAppIcon sx= {isMobile? 'background: #00B314; color: white; height: 7.5vw; width: 7.5vw;' : 'background: #00B314; color: white; height: 2.5vw; width: 2.5vw;'
+        }/>
+    </div>
+  )
 
 }
