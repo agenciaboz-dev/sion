df7d54dce137706b717a71dde4a6574719823664
nandobfer <nandobfer@yahoo.com.br>
2023-05-22 15:59:07 -0300

    feat: added contract list fetch endpoint

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -24,6 +24,14 @@ export const useApi = () => {
                 .catch((error) => defaultError(error, options.errorCallback))
                 .finally(() => defaultFinally(options.finallyCallback))
         },
+        contracts: {
+            list: (options: ApiOptions) => {
+                api.get("/contracts", options.data)
+                    .then((response) => options.callback(response))
+                    .catch((error) => defaultError(error, options.errorCallback))
+                    .finally(() => defaultFinally(options.finallyCallback))
+            },
+        },
         // user: {
         //     update: (options: ApiOptions) => {
         //         api.post("/user", options.data)
