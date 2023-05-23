b2fd2edbbe5f9bf5b39457ab00943f6eb2816121
tainarasn <tainarasn.sba@gmail.com>
2023-05-23 12:44:02 -0300

    feat: created inputs infoComplement and update input for textfield

--- src/pages/Dashboard/NewSeller.tsx
+++ src/pages/Dashboard/NewSeller.tsx
@@ -4,26 +4,39 @@ import './style.scss';
 import {Form, Formik} from 'formik';
 
 interface NewSellerProps {
-    
 }
+
 
 export const NewSeller:React.FC<NewSellerProps> = ({  }) => {
     
+
+
     return (
-        <div className='NewSeller-Component' >
-            <span>Nome Completo</span>
-            <TextField label="" placeholder="Nome Completo" value=""/>
-            <span>RG</span>
-            <TextField label="" placeholder="000.000.000-00" value=""/>
-            <span>CPF</span>
-            <TextField label="" placeholder="000.000.000-00" value=""/>
-            <span>Data de Nascimento</span>
-            <TextField label="" placeholder="dd/mm/aa" value=""/>
-            <span>E-mail</span>
-            <TextField label="" placeholder="Objeto Social" value=""/>
-            <span>Telefone</span>
-            <TextField label="" placeholder="(DDD)9 9999-9999" value=""/>
+        <div className='NewSeller-Component' > 
+
+            <div className='form'>
+                <div className="infoSellers">
+                    <TextField label="Nome completo" placeholder="Nome Completo" value=""/>
+                    <TextField label="RG" placeholder="000.000.000-00" value=""/>
+                    <TextField label="CPF" placeholder="000.000.000-00" value=""/>
+                    <TextField label="Data de Nascimento" placeholder="dd/mm/aa" value=""/>
+                    <TextField label="E-mail" placeholder="Objeto Social" value=""/>
+                    <TextField label="Telefone" placeholder="(DDD)9 9999-9999" value=""/>
+                </div>
+                
+                <div className="infoComplement">
+                    <TextField label="Login" placeholder="Login" value=""/>
+                    <TextField label="Senha" placeholder="Senha" value=""/>
+                    <TextField label="Endereço Residencial" placeholder="Rua Exemplo" value=""/>
+                    <TextField label="Número" placeholder="123" value=""/>
+                    <TextField label="CEP (opcional)" placeholder="00000-000" value=""/>
+                    <TextField label="Bairro (opcional)" placeholder="Exemplo" value=""/>
+                
+                </div>
+            </div>
             
         </div>
+
+        
     )
 }
