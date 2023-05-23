646a2e5534ffa354ff3fd67180660271a047dd5a
nandobfer <nandobfer@yahoo.com.br>
2023-05-23 15:04:29 -0300

    feat: added multi build script

--- build.ps1
+++ build.ps1
@@ -1,4 +1,8 @@
+$user = "cooperativasion"
+$subdomain = "painel.cooperativasion.com.br"
+$path = "/home/$user/$subdomain"
+
 yarn build
 Write-Output 'Uploading build to server'
-scp -r -P 22022 build/* agenciaboz:/home/cooperativasion/vendas.cooperativasion.com.br
-ssh -p 22022 agenciaboz "chown -R cooperativasion:cooperativasion /home/cooperativasion/vendas.cooperativasion.com.br/*"
+scp -r -P 22022 build/* agenciaboz:$path
+ssh -p 22022 agenciaboz "chown -R $user`:$user $path/*"
