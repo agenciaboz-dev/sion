bb2c27a7fbf00b835fc96fbc11d06c983c3ed6e2
tainarasn <tainarasn.sba@gmail.com>
2023-05-23 10:28:23 -0300

    update: update Rate inserted formik

--- src/pages/Dashboard/Rate.tsx
+++ src/pages/Dashboard/Rate.tsx
@@ -1,22 +1,34 @@
 import Button from '@mui/material/Button';
 import React from 'react';
 import './style.scss';
+import TextField from '@mui/material/TextField';
+import { Form, Formik } from 'formik';
 
 interface RateProps {}
 
+interface FormValues {
+    rate: number
+}
+
 export const Rate:React.FC<RateProps> = ({}) => {
 
-    const saveRate = ()=> {
-        console.log("opa")
-    }  
+    const initialValues:FormValues = { rate: 0 }
 
+    const saveRate = (values: FormValues) => {
+        console.log(values)
+    }
+    
     return (
         <div className="Rate-Component" >
-            <p className="title">Tarifa de Energia</p>
-            <input type="number" className="rate" />
-            <Button onClick={saveRate} variant="contained">
-                    Salvar
-            </Button>
+            <span> Tarifa de Energia </span>
+            <Formik initialValues={initialValues} onSubmit = {saveRate}>
+                {({values, handleChange})=>(
+                    <Form>
+                        <TextField label= "Tarifa de energia em %" name="rate" value={values.rate} onChange={handleChange} />
+                        <Button  variant="contained">Salvar</Button>
+                    </Form>
+                )}
+            </Formik>
         </div>
     )
 }
