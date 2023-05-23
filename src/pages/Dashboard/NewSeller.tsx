df3ec760d9dc873aa97488b58868319ebd05018e
tainarasn <tainarasn.sba@gmail.com>
2023-05-23 12:15:48 -0300

    feat: created form infos new seller

--- src/pages/Dashboard/NewSeller.tsx
+++ src/pages/Dashboard/NewSeller.tsx
@@ -1,5 +1,7 @@
+import TextField from '@mui/material/TextField';
 import React from 'react';
 import './style.scss';
+import {Form, Formik} from 'formik';
 
 interface NewSellerProps {
     
@@ -9,6 +11,18 @@ export const NewSeller:React.FC<NewSellerProps> = ({  }) => {
     
     return (
         <div className='NewSeller-Component' >
+            <span>Nome Completo</span>
+            <TextField label="" placeholder="Nome Completo" value=""/>
+            <span>RG</span>
+            <TextField label="" placeholder="000.000.000-00" value=""/>
+            <span>CPF</span>
+            <TextField label="" placeholder="000.000.000-00" value=""/>
+            <span>Data de Nascimento</span>
+            <TextField label="" placeholder="dd/mm/aa" value=""/>
+            <span>E-mail</span>
+            <TextField label="" placeholder="Objeto Social" value=""/>
+            <span>Telefone</span>
+            <TextField label="" placeholder="(DDD)9 9999-9999" value=""/>
             
         </div>
     )
