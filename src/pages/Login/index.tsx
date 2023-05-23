717490ba2f774b39fb477cc5f5c9d20aa8e6b3ab
nandobfer <nandobfer@yahoo.com.br>
2023-05-23 15:19:40 -0300

    update: removed legacy components

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,15 +1,12 @@
-import { Button, Checkbox, FormControlLabel } from "@mui/material"
+import { Button, Checkbox, CircularProgress, FormControlLabel, TextField } from "@mui/material"
 import { Form, Formik } from "formik"
 import { useEffect, useLayoutEffect, useState } from "react"
 import { Footer } from "../../components/Footer"
-import { InputField } from "../../components/InputField"
 import { useLocalStorage } from "../../hooks/useLocalStorage"
 import { ReactComponent as LogoEscuro } from "../../images/logo_bonita.svg"
 import "./style.scss"
 import { useUser } from "../../hooks/useUser"
 import { useNavigate, useParams } from "react-router-dom"
-import { MuiLoading } from "../../components/MuiLoading"
-import { Header } from "../../components/Header"
 import { User } from "../../definitions/user"
 import { useApi } from "../../hooks/useApi"
 
@@ -62,31 +59,31 @@ export const Login = () => {
                         <Form>
                             <div className="user-input-container">
                                 <label>Nome de usuário ou e-mail</label>
-                                <InputField
-                                    title={""}
-                                    id={"user"}
-                                    handleChange={handleChange}
+                                <TextField
+                                    name={"user"}
+                                    onChange={handleChange}
                                     value={values.user}
                                     error={Boolean(errors.user)}
-                                    errorText={errors.user}
+                                    helperText={errors.user}
                                 />
                             </div>
                             <div className="password-input-container">
                                 <label>Senha</label>
-                                <InputField
-                                    title={""}
+                                <TextField
                                     type="password"
-                                    id={"password"}
-                                    handleChange={handleChange}
+                                    name={"password"}
+                                    onChange={handleChange}
                                     value={values.password}
                                     error={error}
-                                    errorText={"Não foi possível fazer login"}
+                                    helperText={"Não foi possível fazer login"}
                                 />
                             </div>
                             <div className="bottom-container">
                                 <p>Perdeu a senha?</p>
                                 <div className="button-container">
-                                    <button type="submit">{loading ? <MuiLoading /> : "Entrar"}</button>
+                                    <button type="submit">
+                                        {loading ? <CircularProgress size="1.5rem" sx={{ color: "white" }} /> : "Entrar"}
+                                    </button>
                                 </div>
                             </div>
                         </Form>
