a2f1cfef1d702de23dd9b1fc252092b43a417cf0
tainarasn <tainarasn.sba@gmail.com>
2023-05-24 11:30:18 -0300

    feat: created loading funciton button

--- src/pages/Dashboard/NewSeller.tsx
+++ src/pages/Dashboard/NewSeller.tsx
@@ -1,15 +1,39 @@
 import TextField from '@mui/material/TextField';
-import React from 'react';
+import React, {useState} from 'react';
 import './style.scss';
 import {Form, Formik} from 'formik';
 import Button from '@mui/material/Button';
+import { useApi } from '../../hooks/useApi';
+import CircularProgress from '@mui/material/CircularProgress/CircularProgress';
 
 interface NewSellerProps {
+}
+
+interface FormValues {
+    name: string
+    rg: string
+    cpf: string
+    birth: string
+    email: string
+    phone: string
+    login: string
+    password: string
+    adress: string
+    number: string
+    cep: string
+    district: string
 }
 
 
 
 export const NewSeller:React.FC<NewSellerProps> = ({  }) => {
+    const api = useApi()
+    const [infoLoading, setInfoLoading] = useState(false)
+
+    const loading_props = {
+        size: "1.5rem",
+        sx: { color: "white" },
+    }
     
     const initialValues = {
         name: '',
@@ -27,26 +51,28 @@ export const NewSeller:React.FC<NewSellerProps> = ({  }) => {
 
     }
 
-    const handleSubmit = () =>{
+    const handleInfoSubmit = (values:FormValues) =>{
+        setInfoLoading(true)
+        console.log(values)
 
     }
 
     return (
         <div className='NewSeller-Component' > 
     
-            <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-            {({values, handleChange})=>(
+            <Formik initialValues={initialValues} onSubmit={handleInfoSubmit} >
+            {({values, handleChange}) => (
                 <Form>
                     <div className='form'>
                     
                         <div className="infoSellers">
                             <p className="data">Dados Pessoais</p>
-                            <TextField label="Nome completo" placeholder="Nome Completo" value=""/>
-                            <TextField label="RG" placeholder="000.000.000-00" value=""/>
-                            <TextField label="CPF" placeholder="000.000.000-00" value=""/>
-                            <TextField label="Data de Nascimento" placeholder="dd/mm/aa" value=""/>
-                            <TextField label="E-mail" placeholder="Objeto Social" value=""/>
-                            <TextField label="Telefone" placeholder="(DDD)9 9999-9999" value=""/>
+                            <TextField name='name' label="Nome completo" value={values.name} onChange={handleChange}/>
+                            <TextField name='rg' label="RG"  placeholder="000.000.000-00" value={values.rg} onChange={handleChange}/>
+                            <TextField name='cpf' label="CPF" placeholder="000.000.000-00" value={values.cpf} onChange={handleChange}/>
+                            <TextField name='birth' label="Data de Nascimento" placeholder="dd/mm/aa" value={values.birth} onChange={handleChange}/>
+                            <TextField name='email' label="E-mail" placeholder="Objeto Social" value={values.email} onChange={handleChange} />
+                            <TextField name='phone' label="Telefone" placeholder="(DDD)9 9999-9999" value={values.phone} onChange={handleChange}/>
                         </div>
                     
                         <div className="infoComplement">
@@ -54,16 +80,16 @@ export const NewSeller:React.FC<NewSellerProps> = ({  }) => {
                             <p className='subtitle'>Foto do rosto do vendedor</p>
                             <div className="biometry">Upload de arquivo </div>
                             <div className="two-inputs">
-                                <TextField label="Login" className='login' placeholder="Login" value=""/>
-                                <TextField label="Senha"  className='login'placeholder="Senha" value=""/>
+                                <TextField name= 'login' label="Login" placeholder="Login" value={values.login} onChange={handleChange}/>
+                                <TextField name= 'password' label="Senha"  placeholder="Senha" value={values.password} onChange={handleChange}/>
                             </div>
                             <div className="two-inputs">
-                                <TextField label="Endereço Residencial" placeholder="Rua Exemplo" value=""/>
-                                <TextField label="Número" placeholder="123" value=""/>
+                                <TextField name= 'adress' label="Endereço Residencial" placeholder="Rua Exemplo" value={values.adress} onChange={handleChange}/>
+                                <TextField name= 'number' label="Número" placeholder="123" value={values.number} onChange={handleChange}/>
                             </div>
                             <div className="two-inputs">
-                                <TextField label="CEP (opcional)" placeholder="00000-000" value=""/>
-                                <TextField label="Bairro (opcional)" placeholder="Exemplo" value=""/>
+                                <TextField name= 'cep' label="CEP (opcional)" placeholder="00000-000" value={values.cep} onChange={handleChange}/>
+                                <TextField name= 'district' label="Bairro (opcional)" placeholder="Exemplo" value={values.district} onChange={handleChange}/>
                             </div>
                         </div>
                     
@@ -88,7 +114,7 @@ export const NewSeller:React.FC<NewSellerProps> = ({  }) => {
                         <div className="biometry"> Upload de arquivo </div>
                     
                     </div>
-                    <Button type='submit' variant="contained"> Cadastrar Vendedor</Button>
+                    <Button type='submit' variant="contained"> {infoLoading ? <CircularProgress {...loading_props}/> : "Cadastrar Vendedor" }</Button>
                 </Form>
                 )}
             </Formik>
