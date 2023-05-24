a38f5d408435a8d5d59ffb59a31c20e429a7ba00
nandobfer <nandobfer@yahoo.com.br>
2023-05-24 16:57:18 -0300

    feat: faq from database

--- src/hooks/useFaq.js
+++ src/hooks/useFaq.js
@@ -1,54 +1,57 @@
+import { useTexts } from "./useTexts"
+
 export const useFaq = () => {
+    const texts = useTexts().faq.slice(2)
     const questions = [
         {
             number: 1,
-            text: 'Quais os benefícios da Cooperativa de Energia?',
-            answer: 'Como cooperado, você terá redução nos custos com energia e receberá energia renovável. Tudo isso com investimento zero e sem alterações físicas no seu negócio.'
+            text: texts[0]?.text,
+            answer: texts[1]?.text,
         },
         {
             number: 2,
-            text: 'Preciso fazer algum investimento?',
-            answer: 'Não. O investimento é zero. A Cooperativa recebe apenas uma taxa de adesão por empresa (CNPJ). Caso sua empresa resolva sair da Cooperativa, esse valor é devolvido integralmente.'
+            text: texts[2]?.text,
+            answer: texts[3]?.text,
         },
         {
             number: 3,
-            text: 'O que é necessário para participar da Cooperativa?',
-            answer: 'É necessário um gasto mensal médio acima de R$2.000,00 em energia.'
+            text: texts[4]?.text,
+            answer: texts[5]?.text,
         },
         {
             number: 4,
-            text: 'Como faço se minha empresa quiser sair da Cooperativa? É cobrado multa?',
-            answer: 'É simples! Pedimos apenas que nos avise com 6 meses de antecedência e nenhuma multa é cobrada. Você volta a receber energia da distribuidora automaticamente.'
+            text: texts[6]?.text,
+            answer: texts[7]?.text,
         },
         {
             number: 5,
-            text: 'Se houver algum problema ou indisponibilidade momentânea na usina, ficarei sem energia?',
-            answer: 'Não. O fornecimento de energia permanece de responsabilidade da distribuidora local (Copel, Energisa, Enel, entre outras). Assim, sua empresa continua a receber energia normalmente mesmo caso haja alguma indisponibilidade momentânea com nossas usinas.'
+            text: texts[8]?.text,
+            answer: texts[9]?.text,
         },
         {
             number: 6,
-            text: 'Se a Cooperativa não gerar os créditos de energia em algum mês, o que acontece?',
-            answer: 'Você pagará sua conta de luz para a distribuidora, com os mesmos valores e da mesma forma que fazia antes de ser um cooperado da Sion Energia.'
+            text: texts[10]?.text,
+            answer: texts[11]?.text,
         },
         {
             number: 7,
-            text: 'Como é feita a divisão e distribuição da energia entre os cooperados?',
-            answer: 'A Sion Cooperativa realiza um processo de análise e otimização (com base no histórico de consumo) para definir qual percentual da energia gerada vai para cada cooperado. Acompanhamos os resultados e, caso seja necessário, alteramos os percentuais. A gestão dos créditos é feita para entregar a maior economia para cada um dos nossos cooperados.'
+            text: texts[12]?.text,
+            answer: texts[13]?.text,
         },
         {
             number: 8,
-            text: 'Haverá uma rede elétrica exclusiva da usina até o meu estabelecimento?',
-            answer: 'Não. A responsabilidade de fornecimento de energia continua com a distribuidora local (Copel, Energisa, Enel, entre outras).'
+            text: texts[14]?.text,
+            answer: texts[15]?.text,
         },
         {
             number: 9,
-            text: 'Precisarei fazer alguma alteração física, obra ou reforma no meu estabelecimento para começar a receber a energia da usina?',
-            answer: 'Não! A energia continua sendo entregue por sua distribuidora local. Nós injetamos a energia na rede da distribuidora e você recebe no seu negócio.'
+            text: texts[16]?.text,
+            answer: texts[17]?.text,
         },
         {
             number: 10,
-            text: 'Meu desconto é o mesmo todos os meses?',
-            answer: 'O desconto na tarifa é fixo. O que pode variar é o percentual da sua conta atendido.'
+            text: texts[18]?.text,
+            answer: texts[19]?.text,
         },
     ]
 
