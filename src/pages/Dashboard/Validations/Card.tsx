b39d86c0c856f8e0e767cbacdc9842f1d0f6657c
tainarasn <tainarasn.sba@gmail.com>
2023-05-29 12:10:53 -0300

    update: informations contracts cards

--- src/pages/Dashboard/Validations/Card.tsx
+++ src/pages/Dashboard/Validations/Card.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import './style.scss';
+import { Contract } from '../../../definitions/contract';
+import {Button} from '@mui/material'
 
 interface CardProps {
-    
+    contract: Contract
 }
 
-export const Card:React.FC<CardProps> = ({  }) => {
+export const Card:React.FC<CardProps> = ({ contract }) => {
     
     return (
         <div className='Card-Component' >
                 <div className="one-column">
                     <div className="info-container">
-                        <p className="name">[Nome do Cliente]/[UC]</p>
+                        <p className="name">{contract.name}/<span>{contract.unit}</span></p>
                         <p className="attach">3 anexos</p>
                         <p className="description">Adicionar descrição</p>
-                        <button className='button' type="submit">[Nome do Vendedor]</button>
+                        <Button variant='contained' className='button' type="submit">{contract.seller.name}</Button>
                     </div>
                 </div>
         </div>
