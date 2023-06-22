2136dca841a0715da30cbba20bd896206662763d
nandobfer <nandobfer@yahoo.com.br>
2023-06-22 16:40:50 -0300

    update: added api2

--- src/api/index.js
+++ src/api/index.js
@@ -5,3 +5,9 @@ export const api = axios.create({
     baseURL: config.api,
     timeout: 1000 * config.timeout,
 })
+
+export const api2 = axios.create({
+    timeout: 1000 * config.timeout,
+    // baseURL: 'https://app.agenciaboz.com.br:4101/api',
+    baseURL: "http://localhost:4101/api",
+})
