76a6451926f9accbafd93398718270b8f7c7730e
nandobfer <nandobfer@yahoo.com.br>
2023-06-23 10:56:30 -0300

    update: styled done page for desktop

--- src/pages/Cadastro/Done/index.jsx
+++ src/pages/Cadastro/Done/index.jsx
@@ -1,17 +1,19 @@
-import { Button } from '@mui/material'
-import './style.scss'
-import CheckCircleIcon from '@mui/icons-material/CheckCircle'
-import { useNavigate } from 'react-router-dom'
+import { Button, useMediaQuery } from "@mui/material"
+import "./style.scss"
+import CheckCircleIcon from "@mui/icons-material/CheckCircle"
+import { useNavigate } from "react-router-dom"
 
 export const Done = () => {
-	const navigate = useNavigate()
-	return (
-		<div className="Done-Component" style={{ flexDirection: 'column', gap: '5vw' }}>
-			<CheckCircleIcon color="primary" sx={{ width: '15vw', height: 'auto' }} />
-			<h1>Seja bem-vindo à Sion Energia!</h1>
-			<Button sx={{ fontSize: '5vw' }} variant="contained" onClick={() => navigate('/cadastro')}>
-				Reiniciar
-			</Button>
-		</div>
-	)
+    const navigate = useNavigate()
+    const isMobile = useMediaQuery("(orientation: portrait)")
+
+    return (
+        <div className="Done-Component" style={{ flexDirection: "column", gap: isMobile ? "5vw" : "1vw" }}>
+            <CheckCircleIcon color="primary" sx={{ width: isMobile ? "15vw" : "5vw", height: "auto" }} />
+            <h1>Seja bem-vindo à Sion Energia!</h1>
+            <Button sx={{ fontSize: isMobile ? "5vw" : "2vw" }} variant="contained" onClick={() => navigate("/cadastro")}>
+                Reiniciar
+            </Button>
+        </div>
+    )
 }
