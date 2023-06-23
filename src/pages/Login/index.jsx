62ce606f8a3e7e49deb73568ea041b3b935d5254
nandobfer <nandobfer@yahoo.com.br>
2023-06-23 11:09:17 -0300

    fix: login header

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -73,59 +73,80 @@ export const Login = () => {
     }, [])
     
     return (
-        <div className='Login-Page' >
+        <div className="Login-Page">
             <Header vendas />
-            <div className="main-container" style={{marginTop: !login && 'auto'}}>
-                <LogoEscuro />
-                <Formik initialValues={{user: '', password: ''}} onSubmit={values => tryLogin(values)}>
-                    {({handleChange, values, submitForm, errors}) => (
+            <div className="main-container" style={{ marginTop: !login && "auto" }}>
+                <Formik initialValues={{ user: "", password: "" }} onSubmit={(values) => tryLogin(values)}>
+                    {({ handleChange, values, submitForm, errors }) => (
                         <Form>
-                             
-                             { params.id &&
-                             <>
-                                <div className="client-button">
-                                        <Button variant='contained' fullWidth onClick={() => navigate('/sign/'+params.id)} >{clientName || <MuiLoading size={'1.5rem'} />}</Button>
-                                    { login && 
-                                    <div className='or'>
-                                        <div>
-                                            <hr />
-                                        </div>
-                                        <p>OU</p>
-                                        <div>
-                                            <hr />
-                                        </div>
+                            {params.id && (
+                                <>
+                                    <div className="client-button">
+                                        <Button variant="contained" fullWidth onClick={() => navigate("/sign/" + params.id)}>
+                                            {clientName || <MuiLoading size={"1.5rem"} />}
+                                        </Button>
+                                        {login && (
+                                            <div className="or">
+                                                <div>
+                                                    <hr />
+                                                </div>
+                                                <p>OU</p>
+                                                <div>
+                                                    <hr />
+                                                </div>
+                                            </div>
+                                        )}
                                     </div>
-                                    }
-
-                                   
-                                </div>
-                                <UpArrowIcon onClick={() => setLogin(!login)} className='up-arrow' style={{transform: login && 'rotate(180deg)'}} />
-                            </>
-                            }
-                            { login &&
+                                    <UpArrowIcon
+                                        onClick={() => setLogin(!login)}
+                                        className="up-arrow"
+                                        style={{ transform: login && "rotate(180deg)" }}
+                                    />
+                                </>
+                            )}
+                            {login && (
                                 <>
                                     <div className="user-input-container">
                                         <label>Nome de usuário ou e-mail</label>
-                                        <InputField title={''} id={'user'} handleChange={handleChange} value={values.user} error={Boolean(errors.user)} errorText={errors.user} />
+                                        <InputField
+                                            title={""}
+                                            id={"user"}
+                                            handleChange={handleChange}
+                                            value={values.user}
+                                            error={Boolean(errors.user)}
+                                            errorText={errors.user}
+                                        />
                                     </div>
                                     <div className="password-input-container">
                                         <label>Senha</label>
-                                        <InputField title={''} type='password' id={'password'} handleChange={handleChange} value={values.password} error={error} errorText={'Não foi possível fazer login'} />
+                                        <InputField
+                                            title={""}
+                                            type="password"
+                                            id={"password"}
+                                            handleChange={handleChange}
+                                            value={values.password}
+                                            error={error}
+                                            errorText={"Não foi possível fazer login"}
+                                        />
                                     </div>
                                     <div className="bottom-container">
-                                        <div className='remember-wrapper'>
-                                            <div className='remember-container'>
-                                                <FormControlLabel control={<Checkbox checked={remind} onChange={() => setRemind(!remind)} />} label={'Lembre de mim'} />
+                                        <div className="remember-wrapper">
+                                            <div className="remember-container">
+                                                <FormControlLabel
+                                                    control={
+                                                        <Checkbox checked={remind} onChange={() => setRemind(!remind)} />
+                                                    }
+                                                    label={"Lembre de mim"}
+                                                />
                                             </div>
                                             <p>Perdeu a senha?</p>
                                         </div>
-                                        <div className='button-container'>
-                                            <button type='submit'>{loading ? <MuiLoading /> : 'Entrar'}</button>
+                                        <div className="button-container">
+                                            <button type="submit">{loading ? <MuiLoading /> : "Entrar"}</button>
                                         </div>
                                     </div>
                                 </>
-                            }
-                            
+                            )}
                         </Form>
                     )}
                 </Formik>
