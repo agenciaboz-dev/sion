231148e1db5e7f1eed88b0184fb2bb62fcf454d7
ispadilha <israel.padilha@gmail.com>
2023-06-23 11:53:25 -0300

    Adjusted footer bg skew

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -44,9 +44,9 @@ export const Footer = ({ vendas }) => {
             marginLeft: '-10vw',
             width: 0, 
             height: 0,
-            borderTop: '35vw solid transparent',
+            borderTop: '15vw solid transparent',
             borderBottom: '0 solid transparent',
-            borderRight: `110vw solid ${COLORS.primary}`,
+            borderRight: `150vw solid ${COLORS.primary}`,
             padding: 0
         }} >
             {location.pathname == '/cadastro/contrato' &&
