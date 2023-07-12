be7c6d31ba9e2f0b1837ec3eb85cc41aed2bd44c
nandobfer <nandobfer@yahoo.com.br>
2023-07-12 12:48:50 -0300

    update * unified api

--- src/api/index.js
+++ src/api/index.js
@@ -2,12 +2,6 @@ import axios from 'axios';
 import config from '../config.json';
 
 export const api = axios.create({
-    baseURL: config.api,
+    baseURL: "https://app.agenciaboz.com.br:4101/api",
     timeout: 1000 * config.timeout,
-})
-
-export const api2 = axios.create({
-    timeout: 1000 * config.timeout,
-    // baseURL: 'https://app.agenciaboz.com.br:4101/api',
-    baseURL: "http://localhost:4101/api",
 })
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -12,55 +12,43 @@ import COLORS from '../../sass/_colors.scss'
 import { useClient } from '../../hooks/useClient';
 
 export const Footer = ({ vendas }) => {
-
     const menus = useHeaderMenus()
     const location = useLocation()
     const navigate = useNavigate()
     const client = useClient()
 
-    const [email, setEmail] = useState('')
+    const [email, setEmail] = useState("")
 
     const finish = () => {
         client.setValue(null)
-        navigate('/')
+        navigate("/")
     }
 
-    const onMailSignUp = useCallback(() => {
-        api.post('/signup', {email, news_signup: true})
-        .then(response => {
-            console.log(response.data)
-
-            if (response.data.insertId) {
-                alert('e-mail cadastrado')
-            } else {
-                alert('erro desconhecido')
-            }
-        })
-    }, [email])
-    
-    return vendas ? 
-    <div className='Footer-Component' style={{
-            backgroundColor: 'transparent',
-            marginLeft: '-10vw',
-            width: 0, 
-            height: 0,
-            borderTop: '15vw solid transparent',
-            borderBottom: '0 solid transparent',
-            borderRight: `150vw solid ${COLORS.primary}`,
-            padding: 0
-        }} >
-            {location.pathname == '/cadastro/contrato' &&
-            <div className="finish-container" onClick={finish} >
-                <p>Finalizar</p>
-                <div className='forward-arrow-container'>
+    return vendas ? (
+        <div
+            className="Footer-Component"
+            style={{
+                backgroundColor: "transparent",
+                marginLeft: "-10vw",
+                width: 0,
+                height: 0,
+                borderTop: "15vw solid transparent",
+                borderBottom: "0 solid transparent",
+                borderRight: `150vw solid ${COLORS.primary}`,
+                padding: 0,
+            }}
+        >
+            {location.pathname == "/cadastro/contrato" && (
+                <div className="finish-container" onClick={finish}>
+                    <p>Finalizar</p>
+                    <div className="forward-arrow-container">
                         <ForwardArrow />
                     </div>
-            </div>
-            }
-    </div>
-    :
-    (
-        <div className='Footer-Component' >
+                </div>
+            )}
+        </div>
+    ) : (
+        <div className="Footer-Component">
             <div className="top-content">
                 <div className="info">
                     <Logo />
@@ -74,14 +62,18 @@ export const Footer = ({ vendas }) => {
                     </div>
                 </div>
                 <div className="links">
-                    {menus.map(menu => {
+                    {menus.map((menu) => {
                         return (
                             <section key={menu.id}>
-                                { location.pathname == '/' ?
-                                    <AnchorLink href={'#'+menu.name} className='menu-title' >{menu.title}</AnchorLink>
-                                    :
-                                    <p onClick={() => navigate('/')} className='menu-title' >{menu.title}</p>
-                                }
+                                {location.pathname == "/" ? (
+                                    <AnchorLink href={"#" + menu.name} className="menu-title">
+                                        {menu.title}
+                                    </AnchorLink>
+                                ) : (
+                                    <p onClick={() => navigate("/")} className="menu-title">
+                                        {menu.title}
+                                    </p>
+                                )}
                             </section>
                         )
                     })}
@@ -90,8 +82,7 @@ export const Footer = ({ vendas }) => {
                     <p className="title">Receba notícias</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     <div className="email-container">
-                        <input type="email" placeholder='Seu e-mail' value={email} onChange={event => setEmail(event.target.value)}/>
-                        <button onClick={onMailSignUp}><span style={{whiteSpace: 'nowrap'}}>Inscrever-se</span></button>
+                        <input type="email" placeholder="Seu e-mail" value={email} onChange={(event) => setEmail(event.target.value)} />
                     </div>
                 </div>
             </div>
@@ -103,7 +94,6 @@ export const Footer = ({ vendas }) => {
                     <FacebookIcon />
                 </div>
             </div>
-            
         </div>
     )
 }
