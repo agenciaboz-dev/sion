501eb0ba2c1c5a55bf2b33ba7e7541ef3cf86add
nandobfer <nandobfer@yahoo.com.br>
2023-07-12 11:04:26 -0300

    feat: added snackbar feedback to boards manipulations

--- src/contexts/boardsContext.tsx
+++ src/contexts/boardsContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, useState } from "react"
 import React from "react"
 import { useIo } from "../hooks/useIo"
+import { useSnackbar } from "burgos-snackbar"
 
 interface BoardsContextValue {
     list: Board[]
@@ -23,24 +24,29 @@ export default BoardsContext
 export const BoardsProvider: React.FC<BoardsProviderProps> = ({ children }) => {
     const io = useIo()
 
+    const { snackbar } = useSnackbar()
+
     const [boards, setBoards] = useState<Board[]>([])
     const [loading, setLoading] = useState(true)
 
     const list = boards.sort((a, b) => a.id - b.id)
     const set = setBoards
     const add = (board: Board, emit = false) => {
-        setBoards([...boards, board])
         if (emit) io.emit("board:new", board)
+        setBoards([...boards, board])
+        snackbar({ severity: emit ? "success" : "info", text: `novo quadro ${board.name}` })
     }
 
     const remove = (board: Board, emit = false) => {
-        setBoards(boards.filter((item) => item.id != board.id))
         if (emit) io.emit("board:remove", board)
+        setBoards(boards.filter((item) => item.id != board.id))
+        snackbar({ severity: emit ? "warning" : "info", text: `quadro ${board.name} removido` })
     }
 
     const update = (board: Board, emit = false) => {
         setBoards([...boards.filter((item) => item.id != board.id), board])
         if (emit) io.emit("board:update", board)
+        snackbar({ severity: emit ? "success" : "info", text: `quadro ${board.name} atualizado` })
     }
 
     io.on("board:new", (board: Board) => add(board))
