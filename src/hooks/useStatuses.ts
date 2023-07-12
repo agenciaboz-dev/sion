5dfc727ef1420f2ef46b173e8867c7bf42cf92d0
nandobfer <nandobfer@yahoo.com.br>
2023-07-12 11:59:55 -0300

    feat: added statuses manipulation broadcast

--- src/hooks/useStatuses.ts
+++ src/hooks/useStatuses.ts
@@ -3,16 +3,11 @@ import StatusesContext from "../contexts/statusesContext"
 
 export const useStatuses = () => {
     const statusesContext = useContext(StatusesContext)
-    const statuses = {
-        list: statusesContext.statuses.sort((a, b) => a.id - b.id),
-        statuses: statusesContext.statuses.sort((a, b) => a.id - b.id),
-        set: statusesContext.setStatuses,
-        loading: statusesContext.loading,
-        setLoading: statusesContext.setLoading,
-        add: (status: Status) => statusesContext.setStatuses([...statusesContext.statuses, status]),
-        remove: (status: Status) => statusesContext.setStatuses(statusesContext.statuses.filter((item) => item.id != status.id)),
-        update: (status: Status) => statusesContext.setStatuses([...statusesContext.statuses.filter((item) => item.id != status.id), status]),
-    }
+    const statuses = statusesContext
 
-    return statuses
+    const add = (status: Status) => statuses.add(status, true)
+    const remove = (status: Status) => statuses.remove(status, true)
+    const update = (status: Status) => statuses.update(status, true)
+
+    return { ...statuses, add, remove, update }
 }
