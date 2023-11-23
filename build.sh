7cdd838998c0da004940488ca9a45566ffb35282
nandobfer <nandobfer@yahoo.com.br>
2023-11-23 15:29:42 -0300

    update: new deploy server

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-user="cooperativasion"
-subdomain="www"
-path="/home/${user}/${subdomain}"
+ssh_profile="root@agencyboz"
+user="coope8746"
+domain="cooperativasion.com.br"
+subdomain="public_html"
+
+path="/home/${domain}/${subdomain}"
 
 yarn build
 echo 'Uploading build to server'
-scp -r -P 22022 build/* agenciaboz:${path}
-ssh -p 22022 agenciaboz "chown -R ${user}:${user} ${path}/*"
+scp -r dist/* ${ssh_profile}:${path}
+ssh ${ssh_profile} "chown -R ${user}:${user} ${path}/*"
