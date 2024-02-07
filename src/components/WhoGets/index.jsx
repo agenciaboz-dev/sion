fad8fb07085a45afd6bdcfe2fa7562e4f8919d2b
ispadilha <israel.padilha@gmail.com>
2024-02-07 17:10:18 -0300

    update: customers images from grid to carousel

--- src/components/WhoGets/index.jsx
+++ src/components/WhoGets/index.jsx
@@ -1,45 +1,56 @@
-import './style.scss';
-import useMeasure from 'react-use-measure';
+import useMeasure from "react-use-measure"
+import { Carousel } from "react-responsive-carousel"
+import { Avatar, Box, useMediaQuery } from "@mui/material"
+import "./style.scss"
+
+const getImageUrl = (customerImage) => `/images/customers/${customerImage}.webp`
+
+const customerImages = ["confiance", "toyota_sulpar", "elasto", "mcdonalds", "baraquias", "varandas", "dutra_ipiranga", "lojasmm"]
 
 export const WhoGets = () => {
-    
-    const [ref, {height}] = useMeasure()
+    const isMobile = useMediaQuery("(max-width:600px)")
+    const [ref, { height }] = useMeasure()
 
     return (
-        <div className='WhoGets-Component' ref={ref} >
+        <div className="WhoGets-Component" ref={ref}>
+            <h2>Quem recebe nossa energia</h2>
             <div className="blue-background"></div>
             <div className="white-background"></div>
 
             <div className="main-container">
-                <div className="customers-images">
-                    <div className="who">
-                        <h1>Quem recebe nossa energia</h1>
-                    </div>
-                    <div className="c-img">
-                        <img src="/images/customers/confiance.webp" alt="" />
-                    </div>
-                    <div className="c-img">
-                        <img src="/images/customers/toyota_sulpar.webp" alt="" />
-                    </div>
-                    <div className="c-img">
-                        <img src="/images/customers/elasto.webp" alt="" />
-                    </div>
-                    <div className="c-img">
-                        <img src="/images/customers/mcdonalds.webp" alt="" />
-                    </div>
-                    <div className="c-img">
-                        <img src="/images/customers/baraquias.webp" alt="" />
-                    </div>
-                    <div className="c-img">
-                        <img src="/images/customers/varandas.webp" alt="" />
-                    </div>
-                    <div className="c-img">                        
-                        <img src="/images/customers/dutra_ipiranga.webp" alt="" />
-                    </div>
-                    <div className="c-img">
-                        <img src="/images/customers/lojasmm.webp" alt="" />
-                    </div>
-                </div>
+                <Carousel
+                    autoPlay
+                    infiniteLoop
+                    interval={3000}
+                    transitionTime={1000}
+                    showThumbs={false}
+                    showStatus={false}
+                    showIndicators={false}
+                    width={"100vw"}
+                >
+                    {customerImages.map((name, index) => (
+                        <Box
+                            key={index}
+                            sx={{
+                                position: "relative",
+                                justifyContent: "center",
+                                padding: "3vw",
+                            }}
+                        >
+                            <Avatar
+                                variant="rounded"
+                                src={getImageUrl(name)}
+                                style={{
+                                    width: isMobile ? "60%" : "25%",
+                                    height: "auto",
+                                    aspectRatio: "1/1",
+                                    borderRadius: "5vw",
+                                    boxShadow: "0 0.5vw 1vw #00000040",
+                                }}
+                            />
+                        </Box>
+                    ))}
+                </Carousel>
             </div>
         </div>
     )
