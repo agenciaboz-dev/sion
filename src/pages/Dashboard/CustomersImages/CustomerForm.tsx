166b5a4a3dd21c6423d39d577923d039ef8a015b
ispadilha <israel.padilha@gmail.com>
2024-02-09 16:04:04 -0300

    update: editing images via panel

--- src/pages/Dashboard/CustomersImages/CustomerForm.tsx
+++ src/pages/Dashboard/CustomersImages/CustomerForm.tsx
@@ -17,15 +17,17 @@ export const CustomerForm: React.FC<CustomerFormProps> = ({}) => {
     const navigate = useNavigate()
     const isNewCustomer = !customer
     const [loading, setLoading] = useState(false)
+    const [deleting, setDeleting] = useState(false)
+    // const [editing, setEditing] = useState(false)
     const { snackbar } = useSnackbar()
 
-    const formik = useFormik<customerForm>({ initialValues: { image: "" }, onSubmit: (values) => createCustomer(values) })
+    const formik = useFormik<customerForm>({ initialValues: customer || { image: "" }, onSubmit: (values) => onSubmit(values) })
 
-    async function createCustomer(values: customerForm) {
+    async function onSubmit(values: customerForm) {
         try {
             console.log(values)
             setLoading(true)
-            const response = await api.post("/customers", values)
+            const response = customer ? await api.patch("/customers", { id: customer.id, image: values.image }) : await api.post("/customers", values)
             setLoading(false)
             navigate(-1)
         } catch (error) {
@@ -35,57 +37,80 @@ export const CustomerForm: React.FC<CustomerFormProps> = ({}) => {
         }
     }
 
-    // const [editing, setEditing] = useState(false)
+    async function deleteCustomerImage() {
+        try {
+            setDeleting(true)
+            const response = await api.delete("/customers", { data: { id: customer?.id } })
+            setDeleting(false)
+            navigate(-1)
+        } catch (error) {
+            console.log(error)
+            setDeleting(false)
+            snackbar({ severity: "error", text: "Erro ao excluir imagem de cliente" })
+        }
+    }
 
     return (
-        <form style={{ display: "contents" }} onSubmit={formik.handleSubmit}>
-            <Box>
-                {!customer && <Avatar sx={{ borderRadius: "0", height: "20vw", width: "20vw" }} />}
-                {customer && <Avatar src={customer.image} sx={{ borderRadius: "0", height: "20vw", width: "20vw" }} />}
-                <Box
-                    sx={{
-                        flexDirection: "column",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        width: "100%",
-                        padding: "2vw",
-                        gap: "2vw",
-                    }}
-                >
-                    <Box>
-                        <TextField value={formik.values.image} name="image" onChange={formik.handleChange} />
-                    </Box>
-                    <Box sx={{ gap: "2vw", justifyContent: "center" }}>
-                        <Button
-                            sx={{ flexDirection: "column", textTransform: "unset", color: "black" }}
-                            onClick={() => navigate(`/dashboard/customers_images`)}
+        <div>
+            <form style={{ display: "contents" }} onSubmit={formik.handleSubmit}>
+                <Box>
+                    {!customer && <Avatar sx={{ borderRadius: "0", height: "20vw", width: "20vw" }} />}
+                    {customer && <Avatar src={customer.image} sx={{ borderRadius: "0", height: "20vw", width: "20vw" }} />}
+                    <Box
+                        sx={{
+                            flexDirection: "column",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            width: "100%",
+                            padding: "2vw",
+                            gap: "2vw",
+                        }}
+                    >
+                        <Box
+                            sx={{
+                                alignItems: "center",
+                                gap: "2vw",
+                            }}
                         >
-                            <ArrowCircleLeftOutlinedIcon sx={{ width: "4vw", height: "4vw", color: "#737373" }} />
-                            <p>Voltar</p>
-                        </Button>
-                        {!isNewCustomer && (
-                            <Button sx={{ flexDirection: "column", textTransform: "unset", color: "black" }}>
-                                <CancelOutlinedIcon sx={{ width: "4vw", height: "4vw", color: "#99143C" }} />
-                                <p>Excluir</p>
+                            <p>URL da imagem:</p>
+                            <TextField value={formik.values.image} name="image" onChange={formik.handleChange} />
+                        </Box>
+                        <Box sx={{ gap: "2vw", justifyContent: "center" }}>
+                            <Button
+                                sx={{ flexDirection: "column", textTransform: "unset", color: "black" }}
+                                onClick={() => navigate(`/dashboard/customers_images`)}
+                            >
+                                <ArrowCircleLeftOutlinedIcon sx={{ width: "4vw", height: "4vw", color: "#737373" }} />
+                                <p>Voltar</p>
                             </Button>
-                        )}
-                        {/* {!isNewCustomer && (
-                                <Button sx={{ flexDirection: "column", textTransform: "unset", color: "black" }} onClick={() => setEditing(!editing)}>
-                                    <CloudCircleOutlinedIcon sx={{ width: "4vw", height: "4vw", color: "#384974" }} />
-                                    <p>Editar</p>
+                            {!isNewCustomer && (
+                                <Button sx={{ flexDirection: "column", textTransform: "unset", color: "black" }} onClick={deleteCustomerImage}>
+                                    {deleting ? (
+                                        <CircularProgress size={"4vw"} />
+                                    ) : (
+                                        <CancelOutlinedIcon sx={{ width: "4vw", height: "4vw", color: "#99143C" }} />
+                                    )}
+                                    <p>Excluir</p>
                                 </Button>
-                            )} */}
-                        <Button type="submit" sx={{ flexDirection: "column", textTransform: "unset", color: "black" }}>
-                            {loading ? (
-                                <CircularProgress size={"4vw"} />
-                            ) : (
-                                <CheckCircleOutlinedIcon sx={{ width: "4vw", height: "4vw", color: "#559F55" }} />
                             )}
-                            Salvar
-                        </Button>
+                            {/* {!isNewCustomer && (
+                                    <Button sx={{ flexDirection: "column", textTransform: "unset", color: "black" }} onClick={() => setEditing(!editing)}>
+                                        <CloudCircleOutlinedIcon sx={{ width: "4vw", height: "4vw", color: "#384974" }} />
+                                        <p>Editar</p>
+                                    </Button>
+                                )} */}
+                            <Button type="submit" sx={{ flexDirection: "column", textTransform: "unset", color: "black" }}>
+                                {loading ? (
+                                    <CircularProgress size={"4vw"} />
+                                ) : (
+                                    <CheckCircleOutlinedIcon sx={{ width: "4vw", height: "4vw", color: "#559F55" }} />
+                                )}
+                                Salvar
+                            </Button>
+                        </Box>
                     </Box>
                 </Box>
-            </Box>
-        </form>
+            </form>
+        </div>
     )
 }
