88747b3e7b0b67392590ce8c5bbc9aa0018726c5
ispadilha <israel.padilha@gmail.com>
2024-02-14 10:47:09 -0300

    update: slick carousel

--- src/components/WhoGets/index.jsx
+++ src/components/WhoGets/index.jsx
@@ -1,9 +1,11 @@
-import useMeasure from "react-use-measure"
-import { Carousel } from "react-responsive-carousel"
+import React, { useEffect, useState } from "react"
 import { Avatar, Box, useMediaQuery } from "@mui/material"
-import "./style.scss"
+import useMeasure from "react-use-measure"
 import axios from "axios"
-import { useEffect, useState } from "react"
+import Slider from "react-slick"
+import "./style.scss"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 
 const getImageUrl = (customerImage) => `/images/customers/${customerImage}.webp`
 
@@ -21,6 +23,20 @@ export const WhoGets = () => {
         fetchCustomerImages()
     }, [])
 
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: isMobile ? 2000 * customerImages.length : 3000 * customerImages.length,
+        slidesToShow: customerImages.length,
+        slidesToScroll: customerImages.length,
+        autoplay: true,
+        autoplaySpeed: 0,
+        cssEase: "linear",
+        variableWidth: true,
+        arrows: true,
+        pauseOnHover: false,
+    }
+
     return (
         <div className="WhoGets-Component" ref={ref}>
             <h2>Quem recebe nossa energia</h2>
@@ -28,39 +44,30 @@ export const WhoGets = () => {
             <div className="white-background"></div>
 
             <div className="main-container">
-                <Carousel
-                    autoPlay
-                    infiniteLoop
-                    interval={3000}
-                    transitionTime={1000}
-                    showThumbs={false}
-                    showStatus={false}
-                    showIndicators={false}
-                    width={"100vw"}
-                    stopOnHover={false}
-                >
+                <Slider {...settings}>
                     {customerImages.map((url, index) => (
                         <Box
                             key={index}
                             sx={{
-                                position: "relative",
+                                padding: isMobile ? "5vw" : "2vw",
                                 justifyContent: "center",
-                                padding: "3vw",
+                                alignItems: "center",
+                                display: "flex !important",
+                                width: isMobile ? "60% !important" : "25% !important",
                             }}
                         >
-                            <Avatar
-                                variant="square"
+                            <img
                                 src={url}
+                                alt={`Customer ${index}`}
                                 style={{
-                                    width: isMobile ? "60%" : "25%",
+                                    width: isMobile ? "60vw" : "15vw",
                                     height: "auto",
-                                    aspectRatio: "1/1",
                                     boxShadow: "0 0.5vw 1vw #00000040",
                                 }}
                             />
                         </Box>
                     ))}
-                </Carousel>
+                </Slider>
             </div>
         </div>
     )
