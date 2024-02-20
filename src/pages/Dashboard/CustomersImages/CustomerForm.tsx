a3ea984d83b76717b9086eded579c4b677c9f88b
ispadilha <israel.padilha@gmail.com>
2024-02-20 16:55:12 -0300

    Starting customer image archive upload

--- src/pages/Dashboard/CustomersImages/CustomerForm.tsx
+++ src/pages/Dashboard/CustomersImages/CustomerForm.tsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react"
-import { Avatar, Box, Button, CircularProgress, TextField } from "@mui/material"
+import { Avatar, Box, Button, CircularProgress, Skeleton, SxProps, TextField } from "@mui/material"
 import { useLocation, useNavigate } from "react-router-dom"
 import ArrowCircleLeftOutlinedIcon from "@mui/icons-material/ArrowCircleLeftOutlined"
 import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined"
 import CloudCircleOutlinedIcon from "@mui/icons-material/CloudCircleOutlined"
 import CheckCircleOutlinedIcon from "@mui/icons-material/CheckCircleOutlined"
+import FileUploadIcon from "@mui/icons-material/FileUpload"
 import { api } from "../../../api"
 import { useFormik } from "formik"
 import { useSnackbar } from "burgos-snackbar"
+import Dropzone from "react-dropzone"
+import "./style.scss"
 
 interface CustomerFormProps {}
 
@@ -20,8 +23,14 @@ export const CustomerForm: React.FC<CustomerFormProps> = ({}) => {
     const [deleting, setDeleting] = useState(false)
     // const [editing, setEditing] = useState(false)
     const { snackbar } = useSnackbar()
-
+    const [hover, setHover] = useState(false)
     const formik = useFormik<customerForm>({ initialValues: customer || { image: "" }, onSubmit: (values) => onSubmit(values) })
+    const skeleton_style = { width: "100%", height: "15vw" }
+    const upload_icon_style: SxProps = {
+        width: "30%",
+        height: "auto",
+        color: "white",
+    }
 
     async function onSubmit(values: customerForm) {
         try {
@@ -58,8 +67,35 @@ export const CustomerForm: React.FC<CustomerFormProps> = ({}) => {
         <div>
             <form style={{ display: "contents" }} onSubmit={formik.handleSubmit}>
                 <Box>
-                    {!customer && <Avatar sx={{ borderRadius: "0", height: "20vw", width: "20vw" }} />}
-                    {customer && <Avatar src={customer.image} sx={{ borderRadius: "0", height: "20vw", width: "20vw" }} />}
+                    <Box
+                        sx={{
+                            flexDirection: "column",
+                        }}
+                    >
+                        <p>Arraste ou selecione um arquivo:</p>
+                        <Dropzone
+                        // onDrop={(acceptedFiles) => fileHandler(acceptedFiles[0])}
+                        >
+                            {({ getRootProps, getInputProps }) => (
+                                <div
+                                    {...getRootProps()}
+                                    className="image-container"
+                                    onMouseEnter={() => setHover(true)}
+                                    onMouseLeave={() => setHover(false)}
+                                >
+                                    <input {...getInputProps()} />
+                                    {!customer && <Avatar sx={{ borderRadius: "0", height: "20vw", width: "20vw" }} />}
+                                    {customer && <Avatar src={customer.image} sx={{ borderRadius: "0", height: "20vw", width: "20vw" }} />}
+                                    {loading && <Skeleton variant="rectangular" sx={skeleton_style} />}
+                                    <div className="icon-container" style={{ opacity: hover ? 1 : 0 }}>
+                                        <FileUploadIcon sx={upload_icon_style} />
+                                    </div>
+                                    <div className="backdrop-overlay" style={{ opacity: hover ? 1 : 0 }}></div>
+                                </div>
+                            )}
+                        </Dropzone>
+                    </Box>
+
                     <Box
                         sx={{
                             flexDirection: "column",
@@ -76,7 +112,7 @@ export const CustomerForm: React.FC<CustomerFormProps> = ({}) => {
                                 gap: "1vw",
                             }}
                         >
-                            <p>URL da imagem:</p>
+                            <p>Ou insira uma URL:</p>
                             <TextField value={formik.values.image} name="image" onChange={formik.handleChange} />
                         </Box>
                         <Box sx={{ gap: "2vw", justifyContent: "center" }}>
